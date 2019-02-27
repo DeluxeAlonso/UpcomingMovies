322ce98e09e1a40ac57f76bd492ceb222b0b8d6c
DeluxeAlonso <alonso.alvarez@pucp.pe>
2019-02-27 00:44:52 -0500

    Creates Movie Reviews test files

---
+++ UpcomingMoviesTests/MovieReviewsTests.swift
@@ -0,0 +1,39 @@
+//
+//  MovieReviewsTests.swift
+//  UpcomingMoviesTests
+//
+//  Created by Alonso on 2/26/19.
+//  Copyright © 2019 Alonso. All rights reserved.
+//
+
+import XCTest
+@testable import UpcomingMovies
+
+class MovieReviewsTests: XCTestCase {
+    
+    private var viewModelToTest: MovieReviewsViewModel!
+    private var movieReviewCellViewModelToTest: MovieReviewCellViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        viewModelToTest = MovieReviewsViewModel(movieId: 1, movieTitle: "Movie 1")
+        movieReviewCellViewModelToTest = MovieReviewCellViewModel(Review.with())
+    }
+    
+    override func tearDown() {
+        viewModelToTest = nil
+        movieReviewCellViewModelToTest = nil
+        super.tearDown()
+    }
+
+}
+
+private final class MockMovieClient: MovieClient {
+    
+    var getReviewResult: Result<ReviewResult?, APIError>?
+    
+    override func getMovieReviews(page: Int, with movieId: Int, completion: @escaping (Result<ReviewResult?, APIError>) -> Void) {
+        completion(getReviewResult!)
+    }
+    
+}
