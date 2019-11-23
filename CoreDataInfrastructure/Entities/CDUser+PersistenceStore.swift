78d49fe68ab045f70b3952f2dda682f52b21d5f0
DeluxeAlonso <alonso.alvarez@pucp.pe>
2019-11-23 11:15:03 -0500

    Adds unit tests for core data infrastructure

--- CoreDataInfrastructure/Entities/CDUser+PersistenceStore.swift
+++ CoreDataInfrastructure/Entities/CDUser+PersistenceStore.swift
@@ -16,13 +16,14 @@ extension PersistenceStore where Entity == CDUser {
         return CDUser.findOrFetch(in: managedObjectContext, matching: predicate)
     }
     
-    func saveUser(_ user: User) {
+    func saveUser(_ user: User, completion: ((Bool) -> Void)? = nil) {
         managedObjectContext.performChanges {
             _ = CDUser.insert(into: self.managedObjectContext,
                               id: user.id,
                               name: user.name,
                               username: user.username,
                               includeAdult: user.includeAdult)
+            completion?(true)
         }
     }
     
