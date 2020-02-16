7466496b42760b666081565b848a95bdab8a618c
DeluxeAlonso <alonso.alvarez@pucp.pe>
2020-02-16 08:05:09 -0500

    Creates SceneDelegate file

---
+++ UpcomingMovies/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  UpcomingMovies
+//
+//  Created by Alonso on 2/16/20.
+//  Copyright © 2020 Alonso. All rights reserved.
+//
+
+import UIKit
+
+@available(iOS 13.0, *)
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    }
+    
+}
