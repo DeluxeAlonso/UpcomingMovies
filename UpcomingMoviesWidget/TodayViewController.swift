390bb98d054e62e0d5f45c07c1a1b00368f86493
DeluxeAlonso <alonso.alvarez@pucp.pe>
2020-05-09 17:49:46 -0500

    Creates Today Extension target

---
+++ UpcomingMoviesWidget/TodayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TodayViewController.swift
+//  UpcomingMoviesWidget
+//
+//  Created by Alonso on 5/9/20.
+//  Copyright © 2020 Alonso. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+        
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
