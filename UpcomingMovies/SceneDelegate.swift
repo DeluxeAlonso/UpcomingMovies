00e34e3933667c6f7780c003a7d19687d32a557d
DeluxeAlonso <alonso.alvarez@pucp.pe>
2020-05-10 15:05:16 -0500

    Fixes scene delegate method

--- UpcomingMovies/SceneDelegate.swift
+++ UpcomingMovies/SceneDelegate.swift
@@ -13,6 +13,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
     
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        if let url = connectionOptions.urlContexts.first?.url {
+            NavigationHandler.handleUrlOpeningNavigation(for: url.absoluteString, and: window)
+        }
+    }
+    
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
         guard let url = URLContexts.first?.url else { return }
         NavigationHandler.handleUrlOpeningNavigation(for: url.absoluteString, and: window)
