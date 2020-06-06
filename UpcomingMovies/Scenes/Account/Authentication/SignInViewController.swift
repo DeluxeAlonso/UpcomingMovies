b31b14c473c152d0f9c34a11b7d071da1071291f
DeluxeAlonso <alonso.alvarez@pucp.pe>
2020-06-06 00:15:46 -0500

    Implements image slider for the sign in screen

--- UpcomingMovies/Scenes/Account/Authentication/SignInViewController.swift
+++ UpcomingMovies/Scenes/Account/Authentication/SignInViewController.swift
@@ -16,9 +16,54 @@ protocol SignInViewControllerDelegate: class {
 
 class SignInViewController: UIViewController {
     
+    @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var signInButton: ShrinkingButton!
     
     weak var delegate: SignInViewControllerDelegate?
+    
+    ///Images we are going to display animated  above the sign in button.
+    private let transitionImages: [UIImage] = [#imageLiteral(resourceName: "SignInLogoFirst"), #imageLiteral(resourceName: "SignInLogoSecond"), #imageLiteral(resourceName: "SignInLogoThird")]
+    private var currentSliderImageIndex = 1
+    weak var sliderTimer: Timer?
+    
+    // MARK: - Lifecycle
+    
+    deinit {
+        sliderTimer?.invalidate()
+        sliderTimer = nil
+    }
+    
+    override func viewDidLoad() {
+        setupSliderTimer()
+    }
+    
+    // MARK: - Private
+    
+    private func setupSliderTimer() {
+        sliderTimer = Timer.scheduledTimer(timeInterval: 2.0,
+                                           target: self,
+                                           selector: #selector(startSliderIconTransition),
+                                           userInfo: nil, repeats: true)
+    }
+    
+    @objc private func startSliderIconTransition() {
+        let image = transitionImages[currentSliderImageIndex]
+        updateSliderimageIndex()
+        UIView.transition(with: self.iconImageView,
+                          duration: 0.5,
+                          options: .transitionCrossDissolve,
+                          animations: {
+          self.iconImageView.image = image
+        }, completion: nil)
+    }
+    
+    private func updateSliderimageIndex() {
+      if (currentSliderImageIndex == transitionImages.count - 1) {
+        currentSliderImageIndex = 0
+      } else {
+        currentSliderImageIndex += 1
+      }
+    }
     
     // MARK: - Public
     
