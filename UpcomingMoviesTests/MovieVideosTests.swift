dea7cb4cb9fe940617a31822469042de12294988
DeluxeAlonso <alonso.alvarez@pucp.pe>
2020-07-09 00:37:14 -0500

    Redesign unit tests

--- UpcomingMoviesTests/MovieVideosTests.swift
+++ UpcomingMoviesTests/MovieVideosTests.swift
@@ -13,34 +13,31 @@ import XCTest
 
 class MovieVideosTests: XCTestCase {
     
-    private var useCaseProvider: MockUseCaseProvider!
     private var movieUseCase: MockMovieUseCase!
-    
-    private var movieVideoCellViewModel: MovieVideoCellViewModel!
+    private var viewModelToTest: MovieVideosViewModelProtocol!
 
     override func setUp() {
         super.setUp()
         movieUseCase = MockMovieUseCase(remoteDataSource: MockInjectionFactory.makeRemoteDataSource().movieDataSource())
-        useCaseProvider = (MockInjectionFactory.useCaseProvider() as! MockUseCaseProvider)
+        
+        let useCaseProvider = (MockInjectionFactory.useCaseProvider() as! MockUseCaseProvider)
         useCaseProvider.mockMovieUseCase = self.movieUseCase
         
-        movieVideoCellViewModel = MovieVideoCellViewModel(Video.with())
+        viewModelToTest = MovieVideosViewModel(movieId: 1,
+                                               movieTitle: "Movie Test",
+                                               useCaseProvider: useCaseProvider)
+        
     }
     
     override func tearDown() {
-        useCaseProvider = nil
         movieUseCase = nil
-        movieVideoCellViewModel = nil
+        viewModelToTest = nil
         super.tearDown()
     }
     
     func testMovieVideosTitle() {
-        //Arrange
-        let viewModel = MovieVideosViewModel(movieId: 1,
-                                             movieTitle: "Movie Test",
-                                             useCaseProvider: useCaseProvider)
         //Act
-        let title = viewModel.movieTitle
+        let title = viewModelToTest.movieTitle
         //Assert
         XCTAssertEqual(title, "Movie Test")
     }
@@ -48,50 +45,53 @@ class MovieVideosTests: XCTestCase {
     func testGetVideosPopulated() {
         //Arrange
         movieUseCase.videos = Result.success([Video.with(id: "1"), Video.with(id: "2")])
-        let viewModel = MovieVideosViewModel(movieId: 1, movieTitle: "Movie 1", useCaseProvider: useCaseProvider)
         //Act
-        viewModel.getMovieVideos()
+        viewModelToTest.getMovieVideos(showLoader: false)
         //Assert
-        XCTAssertEqual(viewModel.viewState.value, .populated([Video.with(id: "1"), Video.with(id: "2")]))
+        XCTAssertEqual(viewModelToTest.viewState.value, .populated([Video.with(id: "1"), Video.with(id: "2")]))
     }
     
     func testGetVideosEmpty() {
         //Arrange
         movieUseCase.videos = Result.success([])
-        let viewModel = MovieVideosViewModel(movieId: 1, movieTitle: "Movie 1", useCaseProvider: useCaseProvider)
         //Act
-        viewModel.getMovieVideos()
+        viewModelToTest.getMovieVideos(showLoader: false)
         //Assert
-        XCTAssertEqual(viewModel.viewState.value, .empty)
+        XCTAssertEqual(viewModelToTest.viewState.value, .empty)
     }
     
     func testGetVideosError() {
         //Arrange
         movieUseCase.videos = Result.failure(APIError.badRequest)
-        let viewModel = MovieVideosViewModel(movieId: 1, movieTitle: "Movie 1", useCaseProvider: useCaseProvider)
         //Act
-        viewModel.getMovieVideos()
+        viewModelToTest.getMovieVideos(showLoader: false)
         //Assert
-        XCTAssertEqual(viewModel.viewState.value, .error(APIError.badRequest))
+        XCTAssertEqual(viewModelToTest.viewState.value, .error(APIError.badRequest))
     }
     
     func testMovieVideoCellName() {
+        //Arrange
+        let cellViewModel = MovieVideoCellViewModel(Video.with())
         //Act
-        let name = movieVideoCellViewModel.name
+        let name = cellViewModel.name
         //Assert
         XCTAssertEqual(name, "Video1")
     }
     
     func testMovieVideoCellKey() {
+        //Arrange
+        let cellViewModel = MovieVideoCellViewModel(Video.with())
         //Act
-        let key = movieVideoCellViewModel.key
+        let key = cellViewModel.key
         //Assert
         XCTAssertEqual(key, "ABC")
     }
     
     func testMovieVideoCellThumbnailURL() {
+        //Arrange
+        let cellViewModel = MovieVideoCellViewModel(Video.with())
         //Act
-        let thumbnailURL = movieVideoCellViewModel.thumbnailURL
+        let thumbnailURL = cellViewModel.thumbnailURL
         //Assert
         XCTAssertEqual(thumbnailURL, URL(string: "https://img.youtube.com/vi/ABC/mqdefault.jpg"))
     }
