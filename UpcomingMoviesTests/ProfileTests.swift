4d1732007d95d2483b4e97d60f50916740c52b92
DeluxeAlonso <alonso.alvarez@pucp.pe>
2020-07-19 17:11:57 -0500

    Adds additional unit tests for Profile scene

--- UpcomingMoviesTests/ProfileTests.swift
+++ UpcomingMoviesTests/ProfileTests.swift
@@ -28,14 +28,15 @@ class ProfileTests: XCTestCase {
     
     override func tearDown() {
         mockInteractor = nil
+        mockFactory = nil
         viewModelToTest = nil
         super.tearDown()
     }
     
-    func testGetAccountDetailInfoReloaded() {
+    func testGetAccountDetailSuccessInfoReloaded() {
         //Arrange
         mockInteractor.getAccountDetailResult = .success(User.with(name: "Alonso"))
-        let expectation = XCTestExpectation(description: "Reload Account Info")
+        let expectation = XCTestExpectation(description: "Reload account info")
         //Act
         viewModelToTest.reloadAccountInfo = {
             expectation.fulfill()
@@ -43,6 +44,74 @@ class ProfileTests: XCTestCase {
         viewModelToTest.getAccountDetails()
         //Assert
         wait(for: [expectation], timeout: 1.0)
+    }
+    
+    func testGetAccountDetailSuccessInfoNotReloaded() {
+        //Arrange
+        mockInteractor.getAccountDetailResult = .success(User.with())
+        let expectation = XCTestExpectation(description: "Should not reload account info")
+        expectation.isInverted = true
+        //Act
+        viewModelToTest.reloadAccountInfo = {
+            expectation.fulfill()
+        }
+        viewModelToTest.getAccountDetails()
+        //Assert
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    func testGetAccountDetailError() {
+        //Arrange
+        mockInteractor.getAccountDetailResult = Result.failure(APIError.badRequest)
+        let expectation = XCTestExpectation(description: "Should not reload account info")
+        expectation.isInverted = true
+        //Act
+        viewModelToTest.reloadAccountInfo = {
+            expectation.fulfill()
+        }
+        viewModelToTest.getAccountDetails()
+        //Assert
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    func testCollectionOptionIndex() {
+        //Arrange
+        let collectionOptionsToTest: [ProfileCollectionOption] = [.favorites, .watchlist]
+        mockFactory.collectionOptions = collectionOptionsToTest
+        //Act
+        let firstCollectionOption = viewModelToTest.collectionOption(at: 0)
+        //Assert
+        XCTAssertEqual(firstCollectionOption.title, collectionOptionsToTest.first?.title)
+    }
+    
+    func testGroupOtionIndex() {
+        //Arrange
+        let groupOptionsToTest: [ProfileGroupOption] = [.customLists]
+        mockFactory.groupOptions = groupOptionsToTest
+        //Act
+        let firstGroupOption = viewModelToTest.groupOption(at: 0)
+        //Assert
+        XCTAssertEqual(firstGroupOption.title, groupOptionsToTest.first?.title)
+    }
+    
+    func testSectionIndex() {
+        //Arrange
+        let sectionsToTest: [ProfileSection] = [.accountInfo, .collections, .groups, .signOut]
+        mockFactory.sections = sectionsToTest
+        //Act
+        let section = viewModelToTest.section(at: 0)
+        //Assert
+        XCTAssertEqual(section, sectionsToTest.first)
+    }
+    
+    func testNumberOfSections() {
+        //Arrange
+        let sectionsToTest: [ProfileSection] = [.accountInfo, .collections, .groups, .signOut]
+        mockFactory.sections = sectionsToTest
+        //Act
+        let numberOfSections = viewModelToTest.numberOfSections()
+        //Assert
+        XCTAssertEqual(numberOfSections, sectionsToTest.count)
     }
 
 }
