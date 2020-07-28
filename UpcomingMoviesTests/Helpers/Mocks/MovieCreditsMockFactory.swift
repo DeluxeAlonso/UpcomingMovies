76f2625ba4501bb5f6b7e1c974e35856a2b17ff1
DeluxeAlonso <alonso.alvarez@pucp.pe>
2020-07-28 13:10:21 -0500

    Creates mock classes for movie credits tests

---
+++ UpcomingMoviesTests/Helpers/Mocks/MovieCreditsMockFactory.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCreditsMockFactory.swift
+//  UpcomingMoviesTests
+//
+//  Created by Alonso on 7/28/20.
+//  Copyright © 2020 Alonso. All rights reserved.
+//
+
+import Foundation
+@testable import UpcomingMovies
+@testable import UpcomingMoviesDomain
+
+class MockMovieCreditsInteractor: MovieCreditsInteractorProtocol {
+    
+    var getMovieCreditsResult: Result<MovieCredits, Error>?
+    func getMovieCredits(for movieId: Int, page: Int?, completion: @escaping (Result<MovieCredits, Error>) -> Void) {
+        completion(getMovieCreditsResult!)
+    }
+    
+}
+
+class MockMovieCreditsFactory: MovieCreditsViewFactoryProtocol {
+    
+    var sections: [MovieCreditsCollapsibleSection] = []
+
+}
