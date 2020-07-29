ba977d0837aa7ffdcf67a4ccf91d5cfadb3471d8
DeluxeAlonso <alonso.alvarez@pucp.pe>
2020-07-28 21:16:55 -0500

    Adds unit tests for movie credits scene

--- UpcomingMoviesTests/MovieCreditsTests.swift
+++ UpcomingMoviesTests/MovieCreditsTests.swift
@@ -67,4 +67,95 @@ class MovieCreditsTests: XCTestCase {
         XCTAssertEqual(viewModelToTest.viewState.value, .error(APIError.badRequest))
     }
     
+    func testNumberOfSections() {
+        //Arrange
+        let sectionsToTest: [MovieCreditsCollapsibleSection] = [
+            MovieCreditsCollapsibleSection(type: .cast, opened: true),
+            MovieCreditsCollapsibleSection(type: .crew, opened: true)
+        ]
+        mockFactory.sections = sectionsToTest
+        //Act
+        let numberOfSections = viewModelToTest.numberOfSections()
+        //Assert
+        XCTAssertEqual(numberOfSections, sectionsToTest.count)
+    }
+    
+    func testNumberOfItemsCastSection() {
+        //Arrange
+        let sectionsToTest: [MovieCreditsCollapsibleSection] = [
+            MovieCreditsCollapsibleSection(type: .cast, opened: true),
+            MovieCreditsCollapsibleSection(type: .crew, opened: true)
+        ]
+        mockFactory.sections = sectionsToTest
+        
+        let castToTest = [Cast.with()]
+        let crewToTest = [Crew.with()]
+        let movieCreditsToTest = MovieCredits(cast: castToTest, crew: crewToTest)
+        mockInteractor.getMovieCreditsResult = Result.success(movieCreditsToTest)
+        //Act
+        viewModelToTest.getMovieCredits(showLoader: false)
+        let numberOfItems = viewModelToTest.numberOfItems(for: 0)
+        //Assert
+        XCTAssertEqual(numberOfItems, castToTest.count)
+    }
+    
+    func testNumberOfItemsCrewSection() {
+        //Arrange
+        let sectionsToTest: [MovieCreditsCollapsibleSection] = [
+            MovieCreditsCollapsibleSection(type: .cast, opened: true),
+            MovieCreditsCollapsibleSection(type: .crew, opened: true)
+        ]
+        mockFactory.sections = sectionsToTest
+        
+        let castToTest = [Cast.with()]
+        let crewToTest = [Crew.with()]
+        let movieCreditsToTest = MovieCredits(cast: castToTest, crew: crewToTest)
+        mockInteractor.getMovieCreditsResult = Result.success(movieCreditsToTest)
+        //Act
+        viewModelToTest.getMovieCredits(showLoader: false)
+        let numberOfItems = viewModelToTest.numberOfItems(for: 1)
+        //Assert
+        XCTAssertEqual(numberOfItems, crewToTest.count)
+    }
+    
+    func testToggleClosedSection() {
+        //Arrange
+        let sectionsToTest: [MovieCreditsCollapsibleSection] = [
+            MovieCreditsCollapsibleSection(type: .cast, opened: false),
+            MovieCreditsCollapsibleSection(type: .crew, opened: false)
+        ]
+        mockFactory.sections = sectionsToTest
+        let sectionIndexToTest = Int.random(in: 0...sectionsToTest.count - 1)
+        let expectation = XCTestExpectation(description: "Should toggle section")
+        //Act
+        viewModelToTest.didToggleSection.bind { section in
+            let headerModel = self.viewModelToTest.headerModel(for: section)
+            XCTAssertTrue(headerModel.opened)
+            expectation.fulfill()
+        }
+        viewModelToTest.toggleSection(sectionIndexToTest)
+        //Assert
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    func testToggleOpenedSection() {
+        //Arrange
+        let sectionsToTest: [MovieCreditsCollapsibleSection] = [
+            MovieCreditsCollapsibleSection(type: .cast, opened: true),
+            MovieCreditsCollapsibleSection(type: .crew, opened: true)
+        ]
+        mockFactory.sections = sectionsToTest
+        let sectionIndexToTest = Int.random(in: 0...sectionsToTest.count - 1)
+        let expectation = XCTestExpectation(description: "Should toggle section")
+        //Act
+        viewModelToTest.didToggleSection.bind { section in
+            let headerModel = self.viewModelToTest.headerModel(for: section)
+            XCTAssertFalse(headerModel.opened)
+            expectation.fulfill()
+        }
+        viewModelToTest.toggleSection(sectionIndexToTest)
+        //Assert
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
 }
