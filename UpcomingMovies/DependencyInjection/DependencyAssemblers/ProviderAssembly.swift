528ec2b5a9df2816e764709a331104e5ec4f8367
DeluxeAlonso <alonso.alvarez@pucp.pe>
2020-09-12 23:15:25 -0500

    Creates Swinject assemblers for datasources and provider

---
+++ UpcomingMovies/DependencyInjection/DependencyAssemblers/ProviderAssembly.swift
@@ -0,0 +1,25 @@
+//
+//  ProviderAssembly.swift
+//  UpcomingMovies
+//
+//  Created by Alonso on 9/12/20.
+//  Copyright © 2020 Alonso. All rights reserved.
+//
+
+import Foundation
+import UpcomingMoviesDomain
+import UpcomingMoviesData
+import CoreDataInfrastructure
+import NetworkInfrastructure
+import Swinject
+
+class ProviderAssembly: Assembly {
+    
+    func assemble(container: Container) {
+        container.register(UseCaseProviderProtocol.self) { resolver in
+            return UseCaseProvider(localDataSource: resolver.resolve(LocalDataSourceProtocol.self)!,
+                                   remoteDataSource: resolver.resolve(RemoteDataSourceProtocol.self)!)
+        }
+    }
+    
+}
