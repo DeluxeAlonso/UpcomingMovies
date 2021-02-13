daac0c85221b3a91a716cf3dead7ea9885d9048a
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-02-13 00:50:00 -0500

    Creates CoreDataStackProtocol

---
+++ CoreDataInfrastructure/CoreDataStack/CoreDataStackProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataStackProtocol.swift
+//  CoreDataInfrastructure
+//
+//  Created by Alonso on 2/13/21.
+//
+
+import CoreData
+
+protocol CoreDataStackProtocol {
+
+    var mainContext: NSManagedObjectContext { get }
+
+}
