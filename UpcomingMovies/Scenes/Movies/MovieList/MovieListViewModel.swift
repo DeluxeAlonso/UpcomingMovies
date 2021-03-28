a34caff4012f1fa5408985b2dc931d76cafae0c7
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-03-27 21:12:24 -0500

    Injects ViewStateHandler in MovieListViewModel

--- UpcomingMovies/Scenes/Movies/MovieList/MovieListViewModel.swift
+++ UpcomingMovies/Scenes/Movies/MovieList/MovieListViewModel.swift
@@ -6,12 +6,16 @@
 //  Copyright © 2019 Alonso. All rights reserved.
 //
 
-import Foundation
 import UpcomingMoviesDomain
 
 final class MovieListViewModel: MovieListViewModelProtocol {
+
+    // MARK: - Dependencies
     
-    var interactor: MoviesInteractorProtocol
+    private let interactor: MoviesInteractorProtocol
+    private let viewStateHandler: ViewStateHandlerProtocol
+
+    // MARK: - Reactive properties
     
     var startLoading: Bindable<Bool> = Bindable(false)
     var viewState: Bindable<SimpleViewState<Movie>> = Bindable(.initial)
@@ -34,11 +38,12 @@ final class MovieListViewModel: MovieListViewModelProtocol {
     
     // MARK: - Initializers
     
-    init(interactor: MoviesInteractorProtocol) {
+    init(interactor: MoviesInteractorProtocol, viewStateHandler: ViewStateHandlerProtocol) {
         self.interactor = interactor
+        self.viewStateHandler = viewStateHandler
     }
     
-    // MARK: - Public
+    // MARK: - MovieListViewModelProtocol
     
     func getMovies() {
         let showLoader = viewState.value.isInitialPage
@@ -48,6 +53,12 @@ final class MovieListViewModel: MovieListViewModelProtocol {
     func refreshMovies() {
         self.fetchMovies(currentPage: 1, showLoader: false)
     }
+
+    func selectedMovie(at index: Int) -> Movie {
+        return movies[index]
+    }
+
+    // MARK: - Private
     
     private func fetchMovies(currentPage: Int, showLoader: Bool = false) {
         startLoading.value = showLoader
@@ -55,26 +66,13 @@ final class MovieListViewModel: MovieListViewModelProtocol {
             self.startLoading.value = false
             switch result {
             case .success(let movies):
-                self.viewState.value = self.processMovieResult(movies,
-                                                               currentPage: currentPage,
-                                                               currentMovies: self.movies)
+                self.viewState.value = self.viewStateHandler.processResult(movies,
+                                                                           currentPage: currentPage,
+                                                                           currentEntities: self.movies)
             case .failure(let error):
                 self.viewState.value = .error(error)
             }
         })
-    }
-    
-    private func processMovieResult(_ movies: [Movie], currentPage: Int,
-                                    currentMovies: [Movie]) -> SimpleViewState<Movie> {
-        var allMovies = currentPage == 1 ? [] : currentMovies
-        allMovies.append(contentsOf: movies)
-        guard !allMovies.isEmpty else { return .empty }
-        
-        return movies.isEmpty ? .populated(allMovies) : .paging(allMovies, next: currentPage + 1)
-    }
-    
-    func selectedMovie(at index: Int) -> Movie {
-        return movies[index]
     }
     
 }
