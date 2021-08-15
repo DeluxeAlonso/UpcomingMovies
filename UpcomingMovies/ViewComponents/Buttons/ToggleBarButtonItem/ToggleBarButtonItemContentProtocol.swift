ebe76ded248e5999d54bf44682cc01a6840b4bdf
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-08-14 23:57:54 -0500

    Creates ToggleBarButtonItemContentProtocol

---
+++ UpcomingMovies/ViewComponents/Buttons/ToggleBarButtonItem/ToggleBarButtonItemContentProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  ToggleBarButtonItemContentProtocol.swift
+//  UpcomingMovies
+//
+//  Created by Alonso on 14/08/21.
+//  Copyright © 2021 Alonso. All rights reserved.
+//
+
+import Foundation
+
+protocol ToggleBarButtonItemContentProtocol {
+    
+}
