1d7261f18bb5a66a759aba2fdfc1edc979bb9be3
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-08-15 00:31:37 -0500

    Removes unnecessary initializer in SpinerLayer

--- UpcomingMovies/ViewComponents/Buttons/ShrinkingButton/SpinerLayer.swift
+++ UpcomingMovies/ViewComponents/Buttons/ShrinkingButton/SpinerLayer.swift
@@ -34,10 +34,6 @@ class SpinerLayer: CAShapeLayer {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    override init(layer: Any) {
-        super.init(layer: layer)
-    }
 
     // MARK: - Internal
     
