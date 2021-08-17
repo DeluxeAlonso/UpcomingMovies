bbcf9120463bace2b8d446564eecf61f06106401
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-08-16 23:23:24 -0500

    Adds docs to CollectionViewDataSourcePrefetching

--- UpcomingMovies/ViewComponents/DataSources/CollectionView Data Sources/CollectionViewDataSourcePrefetching.swift
+++ UpcomingMovies/ViewComponents/DataSources/CollectionView Data Sources/CollectionViewDataSourcePrefetching.swift
@@ -13,13 +13,17 @@ final class CollectionViewDataSourcePrefetching: NSObject, DataSourcePrefetching
     let cellCount: Int
     let needsPrefetch: Bool
     let prefetchHandler: (() -> Void)
+
+    // MARK: - Initializers
     
     init(cellCount: Int, needsPrefetch: Bool, prefetchHandler: @escaping (() -> Void)) {
         self.cellCount = cellCount
         self.needsPrefetch = needsPrefetch
         self.prefetchHandler = prefetchHandler
     }
-    
+
+    // MARK: - UICollectionViewDataSourcePrefetching
+
     func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
         prefetchIfNeeded(for: indexPaths)
     }
