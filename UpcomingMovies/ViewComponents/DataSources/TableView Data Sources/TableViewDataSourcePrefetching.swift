94be6928fe0aaab324ed9821a63193878d368ac9
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-08-16 23:24:28 -0500

    Adds docs to TableViewDataSourcePrefetching

--- UpcomingMovies/ViewComponents/DataSources/TableView Data Sources/TableViewDataSourcePrefetching.swift
+++ UpcomingMovies/ViewComponents/DataSources/TableView Data Sources/TableViewDataSourcePrefetching.swift
@@ -13,12 +13,16 @@ final class TableViewDataSourcePrefetching: NSObject, DataSourcePrefetching, UIT
     let cellCount: Int
     let needsPrefetch: Bool
     let prefetchHandler: (() -> Void)
+
+    // MARK: - Initializers
     
     init(cellCount: Int, needsPrefetch: Bool, prefetchHandler: @escaping (() -> Void)) {
         self.cellCount = cellCount
         self.needsPrefetch = needsPrefetch
         self.prefetchHandler = prefetchHandler
     }
+
+    // MARK: - UITableViewDataSourcePrefetching
     
     func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
         prefetchIfNeeded(for: indexPaths)
