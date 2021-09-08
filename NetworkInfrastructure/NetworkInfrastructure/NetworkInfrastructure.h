1af1b7221c5e2b097e8d12a889e217b55d862e4f
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-09-07 23:52:27 -0500

    Fixes minor syntax issues

--- NetworkInfrastructure/NetworkInfrastructure/NetworkInfrastructure.h
+++ NetworkInfrastructure/NetworkInfrastructure/NetworkInfrastructure.h
@@ -14,5 +14,3 @@ FOUNDATION_EXPORT double NetworkInfrastructureVersionNumber;
 FOUNDATION_EXPORT const unsigned char NetworkInfrastructureVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <NetworkInfrastructure/PublicHeader.h>
-
-
