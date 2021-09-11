c13b56ef2d1410c5a8b826df48762047b3de902a
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-09-10 23:51:22 -0500

    Adds request token tests for success decoding

--- NetworkInfrastructure/NetworkInfrastructureTests/RequestTokenTests.swift
+++ NetworkInfrastructure/NetworkInfrastructureTests/RequestTokenTests.swift
@@ -19,13 +19,44 @@ class RequestTokenTests: XCTestCase {
         XCTAssertThrowsError(try JSONDecoder().decode(RequestTokenResult.self, from: jsonDataToTest))
     }
 
-    func testMissingTokenFromDecodedAccessToken() throws {
+    func testMissingTokenFromDecodedRequestToken() throws {
         // Arrange
         let dataResponse = MockResponse.requestToken.dataResponse
         // Act
         let jsonDataToTest = try dataResponse.json(deletingKeyPaths: "request_token")
         // Assert
         XCTAssertThrowsError(try JSONDecoder().decode(RequestTokenResult.self, from: jsonDataToTest))
+    }
+
+    func testMissingSuccessAndTokenFromDecodedRequestToken() throws {
+        // Arrange
+        let dataResponse = MockResponse.requestToken.dataResponse
+        // Act
+        let jsonDataToTest = try dataResponse.json(deletingKeyPaths: "success", "request_token")
+        // Assert
+        XCTAssertThrowsError(try JSONDecoder().decode(AccessToken.self, from: jsonDataToTest))
+    }
+
+    func testSuccessFromDecodedRequestToken() throws {
+        // Arrange
+        let successToTest = true
+        let dataResponse = MockResponse.requestToken.dataResponse
+        // Act
+        let jsonDataToTest = try dataResponse.json(updatingKeyPaths: ("success", successToTest))
+        let decodedRequestToken = try JSONDecoder().decode(RequestTokenResult.self, from: jsonDataToTest)
+        // Assert
+        XCTAssertEqual(decodedRequestToken.success, successToTest)
+    }
+
+    func testTokenFromDecodedRequestToken() throws {
+        // Arrange
+        let tokenToTest = "test"
+        let dataResponse = MockResponse.requestToken.dataResponse
+        // Act
+        let jsonDataToTest = try dataResponse.json(updatingKeyPaths: ("request_token", tokenToTest))
+        let decodedRequestToken = try JSONDecoder().decode(RequestTokenResult.self, from: jsonDataToTest)
+        // Assert
+        XCTAssertEqual(decodedRequestToken.token, tokenToTest)
     }
 
 }
