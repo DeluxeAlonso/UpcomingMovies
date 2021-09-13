e120a28bab75101f5fa838f34ab18b60402c15dd
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-09-12 23:53:36 -0500

    Adds unit tests for favorite missing keys

--- NetworkInfrastructure/NetworkInfrastructureTests/FavoriteTests.swift
+++ NetworkInfrastructure/NetworkInfrastructureTests/FavoriteTests.swift
@@ -6,27 +6,35 @@
 //
 
 import XCTest
+@testable import NetworkInfrastructure
 
 class FavoriteTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testMissingStatusCodeFromResponse() throws {
+        // Arrange
+        let dataResponse = MockResponse.markAsFavorite.dataResponse
+        // Act
+        let jsonDataToTest = try dataResponse.json(deletingKeyPaths: "status_code")
+        // Assert
+        XCTAssertThrowsError(try JSONDecoder().decode(MarkAsFavoriteResult.self, from: jsonDataToTest))
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testMissingStatusMessageFromResponse() throws {
+        // Arrange
+        let dataResponse = MockResponse.markAsFavorite.dataResponse
+        // Act
+        let jsonDataToTest = try dataResponse.json(deletingKeyPaths: "status_message")
+        // Assert
+        XCTAssertThrowsError(try JSONDecoder().decode(MarkAsFavoriteResult.self, from: jsonDataToTest))
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testMissingStatusCodeAndMessageFromResponse() throws {
+        // Arrange
+        let dataResponse = MockResponse.markAsFavorite.dataResponse
+        // Act
+        let jsonDataToTest = try dataResponse.json(deletingKeyPaths: "status_code", "status_message")
+        // Assert
+        XCTAssertThrowsError(try JSONDecoder().decode(MarkAsFavoriteResult.self, from: jsonDataToTest))
     }
 
 }
