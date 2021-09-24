85a7b4ea9d5a4201e9f1d7d93313a5063e443d29
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-09-24 00:48:01 -0500

    Adds tests for missing keys in review response

--- NetworkInfrastructure/NetworkInfrastructureTests/MovieReviewTests.swift
+++ NetworkInfrastructure/NetworkInfrastructureTests/MovieReviewTests.swift
@@ -6,27 +6,35 @@
 //
 
 import XCTest
+@testable import NetworkInfrastructure
 
 class MovieReviewTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testMissingIdFromResponse() throws {
+        // Arrange
+        let dataResponse = MockResponse.review.dataResponse
+        // Act
+        let jsonDataToTest = try dataResponse.json(deletingKeyPaths: "id")
+        // Assert
+        XCTAssertThrowsError(try JSONDecoder().decode(Review.self, from: jsonDataToTest))
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testMissingAuthorFromResponse() throws {
+        // Arrange
+        let dataResponse = MockResponse.review.dataResponse
+        // Act
+        let jsonDataToTest = try dataResponse.json(deletingKeyPaths: "author")
+        // Assert
+        XCTAssertThrowsError(try JSONDecoder().decode(Review.self, from: jsonDataToTest))
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testMissingContentFromResponse() throws {
+        // Arrange
+        let dataResponse = MockResponse.review.dataResponse
+        // Act
+        let jsonDataToTest = try dataResponse.json(deletingKeyPaths: "content")
+        // Assert
+        XCTAssertThrowsError(try JSONDecoder().decode(Review.self, from: jsonDataToTest))
     }
 
 }
