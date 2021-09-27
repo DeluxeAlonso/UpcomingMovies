dadad1ed065baa0ebeb4f671cf97d67e4a2a3602
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-09-27 00:42:30 -0500

    Adds unit tests for rate movie success reponse

--- NetworkInfrastructure/NetworkInfrastructureTests/RateMovieTests.swift
+++ NetworkInfrastructure/NetworkInfrastructureTests/RateMovieTests.swift
@@ -27,5 +27,27 @@ class RateMovieTests: XCTestCase {
         // Assert
         XCTAssertThrowsError(try JSONDecoder().decode(RateMovieResult.self, from: jsonDataToTest))
     }
+
+    func testStatusCodeFromResponse() throws {
+        // Arrange
+        let statusCodeToTest = 200
+        let dataResponse = MockResponse.rateMovie.dataResponse
+        // Act
+        let jsonDataToTest = try dataResponse.json(updatingKeyPaths: ("status_code", statusCodeToTest))
+        let decodedRateMovieResult = try JSONDecoder().decode(RateMovieResult.self, from: jsonDataToTest)
+        // Assert
+        XCTAssertEqual(decodedRateMovieResult.statusCode, statusCodeToTest)
+    }
+
+    func testStatusMessageFromResponse() throws {
+        // Arrange
+        let statusMessageToTest = "statusMessage"
+        let dataResponse = MockResponse.rateMovie.dataResponse
+        // Act
+        let jsonDataToTest = try dataResponse.json(updatingKeyPaths: ("status_message", statusMessageToTest))
+        let decodedRateMovieResult = try JSONDecoder().decode(RateMovieResult.self, from: jsonDataToTest)
+        // Assert
+        XCTAssertEqual(decodedRateMovieResult.statusMessage, statusMessageToTest)
+    }
     
 }
