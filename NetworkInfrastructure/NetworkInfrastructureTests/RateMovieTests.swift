2a1f0d80e0ddd34b489e7cd6bb39701e3136b79b
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-09-27 00:38:53 -0500

    Adds unit tests for rate movie response missing keys

--- NetworkInfrastructure/NetworkInfrastructureTests/RateMovieTests.swift
+++ NetworkInfrastructure/NetworkInfrastructureTests/RateMovieTests.swift
@@ -6,27 +6,26 @@
 //
 
 import XCTest
+@testable import NetworkInfrastructure
 
 class RateMovieTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testMissingStatusCodeFromResponse() throws {
+        // Arrange
+        let dataResponse = MockResponse.rateMovie.dataResponse
+        // Act
+        let jsonDataToTest = try dataResponse.json(deletingKeyPaths: "status_code")
+        // Assert
+        XCTAssertThrowsError(try JSONDecoder().decode(RateMovieResult.self, from: jsonDataToTest))
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testMissingStatusMessageFromResponse() throws {
+        // Arrange
+        let dataResponse = MockResponse.rateMovie.dataResponse
+        // Act
+        let jsonDataToTest = try dataResponse.json(deletingKeyPaths: "status_message")
+        // Assert
+        XCTAssertThrowsError(try JSONDecoder().decode(RateMovieResult.self, from: jsonDataToTest))
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
