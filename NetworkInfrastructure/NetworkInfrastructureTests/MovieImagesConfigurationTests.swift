2a996e845fc3d0b374cf96a6e9cfb1709d294c21
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-09-29 23:32:22 -0500

    Adds tests for missing keys in image configuration response

--- NetworkInfrastructure/NetworkInfrastructureTests/MovieImagesConfigurationTests.swift
+++ NetworkInfrastructure/NetworkInfrastructureTests/MovieImagesConfigurationTests.swift
@@ -6,27 +6,35 @@
 //
 
 import XCTest
+@testable import NetworkInfrastructure
 
 class MovieImagesConfigurationTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testMissingBaseURLStringFromResponse() throws {
+        // Arrange
+        let dataResponse = MockResponse.imageConfiguration.dataResponse
+        // Act
+        let jsonDataToTest = try dataResponse.json(deletingKeyPaths: "secure_base_url")
+        // Assert
+        XCTAssertThrowsError(try JSONDecoder().decode(ImagesConfiguration.self, from: jsonDataToTest))
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testMissingBackdropSizesFromResponse() throws {
+        // Arrange
+        let dataResponse = MockResponse.imageConfiguration.dataResponse
+        // Act
+        let jsonDataToTest = try dataResponse.json(deletingKeyPaths: "backdrop_sizes")
+        // Assert
+        XCTAssertThrowsError(try JSONDecoder().decode(ImagesConfiguration.self, from: jsonDataToTest))
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testMissingPosterSizesFromResponse() throws {
+        // Arrange
+        let dataResponse = MockResponse.imageConfiguration.dataResponse
+        // Act
+        let jsonDataToTest = try dataResponse.json(deletingKeyPaths: "poster_sizes")
+        // Assert
+        XCTAssertThrowsError(try JSONDecoder().decode(ImagesConfiguration.self, from: jsonDataToTest))
     }
 
 }
