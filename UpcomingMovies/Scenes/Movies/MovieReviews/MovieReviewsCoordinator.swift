8fbfff46b7ff49145587e2f4875dd06716e740f6
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-10-15 01:21:45 -0500

    Updates UpcomingMoviesNavigation transition

--- UpcomingMovies/Scenes/Movies/MovieReviews/MovieReviewsCoordinator.swift
+++ UpcomingMovies/Scenes/Movies/MovieReviews/MovieReviewsCoordinator.swift
@@ -28,7 +28,7 @@ final class MovieReviewsCoordinator: NSObject, Coordinator, MovieReviewsCoordina
         viewController.viewModel = DIContainer.shared.resolve(arguments: movieId, movieTitle)
         viewController.coordinator = self
         
-        navigationController.delegate = self
+        //navigationController.delegate = self
         navigationController.pushViewController(viewController, animated: true)
     }
     
@@ -49,18 +49,18 @@ final class MovieReviewsCoordinator: NSObject, Coordinator, MovieReviewsCoordina
 
 // MARK: - UINavigationControllerDelegate
 
-extension MovieReviewsCoordinator: UINavigationControllerDelegate {
-    
-    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
-        guard let fromViewController = navigationController.transitionCoordinator?.viewController(forKey: .from) else {
-            return
-        }
-        // Check whether our view controller array already contains that view controller.
-        // If it does it means we’re pushing a different view controller on top rather than popping it, so exit.
-        if navigationController.viewControllers.contains(fromViewController) {
-            return
-        }
-        unwrappedParentCoordinator.childDidFinish()
-    }
-    
-}
+//extension MovieReviewsCoordinator: UINavigationControllerDelegate {
+//
+//    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+//        guard let fromViewController = navigationController.transitionCoordinator?.viewController(forKey: .from) else {
+//            return
+//        }
+//        // Check whether our view controller array already contains that view controller.
+//        // If it does it means we’re pushing a different view controller on top rather than popping it, so exit.
+//        if navigationController.viewControllers.contains(fromViewController) {
+//            return
+//        }
+//        unwrappedParentCoordinator.childDidFinish()
+//    }
+//
+//}
