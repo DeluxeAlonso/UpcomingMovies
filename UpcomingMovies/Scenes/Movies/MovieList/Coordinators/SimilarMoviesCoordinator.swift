b0827b6d345722aa9f98296edabbfc5cb2467fc2
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-10-15 23:42:44 -0500

    Removes unnecessary UINavigationControllerDelegate conformance

--- UpcomingMovies/Scenes/Movies/MovieList/Coordinators/SimilarMoviesCoordinator.swift
+++ UpcomingMovies/Scenes/Movies/MovieList/Coordinators/SimilarMoviesCoordinator.swift
@@ -9,7 +9,7 @@
 import UIKit
 import UpcomingMoviesDomain
 
-final class SimilarMoviesCoordinator: NSObject, MovieListCoordinatorProtocol, Coordinator, MovieDetailCoordinable, UINavigationControllerDelegate {
+final class SimilarMoviesCoordinator: NSObject, MovieListCoordinatorProtocol, Coordinator, MovieDetailCoordinable {
     
     var childCoordinators: [Coordinator] = []
     var parentCoordinator: Coordinator?
@@ -27,21 +27,8 @@ final class SimilarMoviesCoordinator: NSObject, MovieListCoordinatorProtocol, Co
         viewController.viewModel = DIContainer.shared.resolve(name: "SimilarMovies",
                                                               arguments: LocalizedStrings.similarMoviesTitle.localized, movieId)
         viewController.coordinator = self
-        
-        navigationController.delegate = self
+
         navigationController.pushViewController(viewController, animated: true)
-    }
-    
-    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
-        guard let fromViewController = navigationController.transitionCoordinator?.viewController(forKey: .from) else {
-            return
-        }
-        // Check whether our view controller array already contains that view controller.
-        // If it does it means we’re pushing a different view controller on top rather than popping it, so exit.
-        if navigationController.viewControllers.contains(fromViewController) {
-            return
-        }
-        unwrappedParentCoordinator.childDidFinish()
     }
     
 }
