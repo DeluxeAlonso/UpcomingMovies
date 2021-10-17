afc1ba3c184a883639cebd87bcb4784bfa34b94c
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-10-16 23:52:07 -0500

    Makes AccountCoordinator conform to UINavigationControllerDelegate protocol

--- UpcomingMovies/Scenes/Account/AccountCoordinator.swift
+++ UpcomingMovies/Scenes/Account/AccountCoordinator.swift
@@ -9,7 +9,7 @@
 import UIKit
 import UpcomingMoviesDomain
 
-final class AccountCoordinator: AccountCoordinatorProtocol, RootCoordinator {
+final class AccountCoordinator: NSObject, AccountCoordinatorProtocol, RootCoordinator {
     
     var childCoordinators: [Coordinator] = []
     var parentCoordinator: Coordinator?
@@ -32,7 +32,8 @@ final class AccountCoordinator: AccountCoordinatorProtocol, RootCoordinator {
         
         viewController.viewModel = DIContainer.shared.resolve()
         viewController.coordinator = self
-        
+
+        navigationController.delegate = self
         navigationController.pushViewController(viewController, animated: true)
     }
     
@@ -142,3 +143,21 @@ final class AccountCoordinator: AccountCoordinatorProtocol, RootCoordinator {
     }
     
 }
+
+// MARK: - UINavigationControllerDelegate
+
+ extension AccountCoordinator: UINavigationControllerDelegate {
+
+     func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+         guard let fromViewController = navigationController.transitionCoordinator?.viewController(forKey: .from) else {
+             return
+         }
+         // Check whether our view controller array already contains that view controller.
+         // If it does it means we’re pushing a different view controller on top rather than popping it, so exit.
+         if navigationController.viewControllers.contains(fromViewController) {
+             return
+         }
+         unwrappedParentCoordinator.childDidFinish()
+     }
+
+ }
