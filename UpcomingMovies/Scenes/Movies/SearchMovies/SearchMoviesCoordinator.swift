1496ca3bbf762f10039f31fc0b611374ca9fd5db
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-10-16 23:23:46 -0500

    Makes SearchMoviesCoordinator conform to UINavigationControllerDelegate protocol

--- UpcomingMovies/Scenes/Movies/SearchMovies/SearchMoviesCoordinator.swift
+++ UpcomingMovies/Scenes/Movies/SearchMovies/SearchMoviesCoordinator.swift
@@ -9,7 +9,7 @@
 import UIKit
 import UpcomingMoviesDomain
 
-final class SearchMoviesCoordinator: SearchMoviesCoordinatorProtocol, RootCoordinator, MovieDetailCoordinable {
+final class SearchMoviesCoordinator: NSObject, SearchMoviesCoordinatorProtocol, RootCoordinator, MovieDetailCoordinable {
     
     var childCoordinators: [Coordinator] = []
     var parentCoordinator: Coordinator?
@@ -31,7 +31,8 @@ final class SearchMoviesCoordinator: SearchMoviesCoordinatorProtocol, RootCoordi
         let viewController = SearchMoviesViewController.instantiate()
         
         viewController.coordinator = self
-        
+
+        navigationController.delegate = self
         navigationController.pushViewController(viewController, animated: true)
     }
     
@@ -108,3 +109,21 @@ final class SearchMoviesCoordinator: SearchMoviesCoordinatorProtocol, RootCoordi
     }
     
 }
+
+// MARK: - UINavigationControllerDelegate
+
+ extension SearchMoviesCoordinator: UINavigationControllerDelegate {
+
+     func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+         guard let fromViewController = navigationController.transitionCoordinator?.viewController(forKey: .from) else {
+             return
+         }
+         // Check whether our view controller array already contains that view controller.
+         // If it does it means we’re pushing a different view controller on top rather than popping it, so exit.
+         if navigationController.viewControllers.contains(fromViewController) {
+             return
+         }
+         unwrappedParentCoordinator.childDidFinish()
+     }
+
+ }
