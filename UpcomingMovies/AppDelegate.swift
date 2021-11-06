51cded7ecea03b044280f20cf037101d330c4e4c
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-11-06 13:02:14 -0500

    Creates an appearance app delegate extension

--- UpcomingMovies/AppDelegate.swift
+++ UpcomingMovies/AppDelegate.swift
@@ -50,24 +50,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    /**
-     Configures UINavigationBar an UITabBar appearance to have a similar behavior as pre-iOS15.
-
-     In iOS 15, UIKit has extended the usage of the scrollEdgeAppearance,
-     which by default produces a transparent background, to all navigation bars.
-     The background is controlled by when your scroll view scrolls
-     content behind the navigation bar.
-     */
-    private func configureGlobalAppearanceIfNeeded() {
-        if #available(iOS 15, *) {
-            let navigationBarAppearance = UINavigationBarAppearance()
-            UINavigationBar.appearance().standardAppearance = navigationBarAppearance
-            UINavigationBar.appearance().scrollEdgeAppearance = UINavigationBar.appearance().standardAppearance
-
-            let tabBarAppearance = UITabBarAppearance()
-            UITabBar.appearance().standardAppearance = tabBarAppearance
-            UITabBar.appearance().scrollEdgeAppearance = UITabBar.appearance().standardAppearance
-        }
-    }
-
 }
