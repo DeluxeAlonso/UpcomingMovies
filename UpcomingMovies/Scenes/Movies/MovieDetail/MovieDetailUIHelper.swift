646d327a5d28164d2ee6a4c806cb7837d6a7a6ed
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-01-26 22:47:13 -0500

    Updates MovieDetailUIHelper to conform LoadingDisplayable protocol

--- UpcomingMovies/Scenes/Movies/MovieDetail/MovieDetailUIHelper.swift
+++ UpcomingMovies/Scenes/Movies/MovieDetail/MovieDetailUIHelper.swift
@@ -6,13 +6,21 @@
 //  Copyright © 2022 Alonso. All rights reserved.
 //
 
-final class MovieDetailUIHelper: MovieDetailUIHelperProtocol {
+final class MovieDetailUIHelper: MovieDetailUIHelperProtocol, LoadingDisplayable {
 
     private let progressHUDAdapter: ProgressHUDAdapterProtocol
+
+    // MARK: - LoadingDisplayable
+
+    var loaderView: LoadingView = RadarView()
+
+    // MARK: - Initializers
 
     init(progressHUDAdapter: ProgressHUDAdapterProtocol) {
         self.progressHUDAdapter = progressHUDAdapter
     }
+
+    // MARK: - MovieDetailUIHelperProtocol
 
     func showHUD(with text: String) {
         progressHUDAdapter.showHUDWithOnlyText(text)
