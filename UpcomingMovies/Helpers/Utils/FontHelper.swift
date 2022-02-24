ed697fc67aab57b3f52e0405a48bc3dcbe146050
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-02-24 00:33:36 -0500

    Removes FontSize enum from FontHelper

--- UpcomingMovies/Helpers/Utils/FontHelper.swift
+++ UpcomingMovies/Helpers/Utils/FontHelper.swift
@@ -10,25 +10,19 @@ import UIKit
 
 struct FontHelper {
 
-    enum FontSize: CGFloat {
-        case small = 14
-        case medium = 15
-        case big = 16
-    }
-
-    static func bold(withSize size: CGFloat) -> UIFont {
+    private static func bold(withSize size: CGFloat) -> UIFont {
         return UIFont.systemFont(ofSize: size, weight: UIFont.Weight.bold)
     }
 
-    static func semiBold(withSize size: CGFloat) -> UIFont {
+    private static func semiBold(withSize size: CGFloat) -> UIFont {
         return UIFont.systemFont(ofSize: size, weight: UIFont.Weight.semibold)
     }
 
-    static func light(withSize size: CGFloat) -> UIFont {
+    private static func light(withSize size: CGFloat) -> UIFont {
         return UIFont.systemFont(ofSize: size, weight: UIFont.Weight.light)
     }
 
-    static func regular(withSize size: CGFloat) -> UIFont {
+    private static func regular(withSize size: CGFloat) -> UIFont {
         return UIFont.systemFont(ofSize: size, weight: UIFont.Weight.regular)
     }
 
@@ -36,28 +30,15 @@ struct FontHelper {
 
 extension FontHelper {
 
-    struct Default {
+    static let headline = FontHelper.semiBold(withSize: 21.0)
+    static let body = FontHelper.regular(withSize: 17.0)
+    static let callout = FontHelper.regular(withSize: 16.0)
 
-        static let headline = FontHelper.semiBold(withSize: 21.0)
-        static let body = FontHelper.regular(withSize: 17.0)
-        static let callout = FontHelper.regular(withSize: 16.0)
+    static let subhead = FontHelper.regular(withSize: 15.0)
+    static let subheadLight = FontHelper.light(withSize: 15.0)
 
-        static let subhead = FontHelper.regular(withSize: 15.0)
-        static let subheadLight = FontHelper.light(withSize: 15.0)
-
-        static let footnote = FontHelper.regular(withSize: 13.0)
-        static let caption1 = FontHelper.regular(withSize: 12.0)
-        static let caption2 = FontHelper.regular(withSize: 11.0)
-
-//        static let smallLight = FontHelper.light(withSize: FontSize.small.rawValue)
-//        static let smallBold = FontHelper.bold(withSize: FontSize.small.rawValue)
-//
-//        static let mediumLight = FontHelper.light(withSize: FontSize.medium.rawValue)
-//        static let mediumBold = FontHelper.bold(withSize: FontSize.medium.rawValue)
-//
-//        static let bigLight = FontHelper.light(withSize: FontSize.big.rawValue)
-//        static let bigBold = FontHelper.bold(withSize: FontSize.big.rawValue)
-
-    }
+    static let footnote = FontHelper.regular(withSize: 13.0)
+    static let caption1 = FontHelper.regular(withSize: 12.0)
+    static let caption2 = FontHelper.regular(withSize: 11.0)
 
 }
