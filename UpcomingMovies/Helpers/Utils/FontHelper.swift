9b36ccdbdcae226c303034868da11567e5e2d1f7
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-02-23 23:02:46 -0500

    Adds more font sizes to font helper

--- UpcomingMovies/Helpers/Utils/FontHelper.swift
+++ UpcomingMovies/Helpers/Utils/FontHelper.swift
@@ -38,14 +38,25 @@ extension FontHelper {
 
     struct Default {
 
-        static let smallLight = FontHelper.light(withSize: FontSize.small.rawValue)
-        static let smallBold = FontHelper.bold(withSize: FontSize.small.rawValue)
+        static let headline = FontHelper.semiBold(withSize: 21.0)
+        static let body = FontHelper.regular(withSize: 17.0)
+        static let callout = FontHelper.regular(withSize: 16.0)
 
-        static let mediumLight = FontHelper.light(withSize: FontSize.medium.rawValue)
-        static let mediumBold = FontHelper.bold(withSize: FontSize.medium.rawValue)
+        static let subhead = FontHelper.regular(withSize: 15.0)
+        static let subheadLight = FontHelper.regular(withSize: 15.0)
 
-        static let bigLight = FontHelper.light(withSize: FontSize.big.rawValue)
-        static let bigBold = FontHelper.bold(withSize: FontSize.big.rawValue)
+        static let footnote = FontHelper.regular(withSize: 13.0)
+        static let caption1 = FontHelper.regular(withSize: 12.0)
+        static let caption2 = FontHelper.regular(withSize: 11.0)
+
+//        static let smallLight = FontHelper.light(withSize: FontSize.small.rawValue)
+//        static let smallBold = FontHelper.bold(withSize: FontSize.small.rawValue)
+//
+//        static let mediumLight = FontHelper.light(withSize: FontSize.medium.rawValue)
+//        static let mediumBold = FontHelper.bold(withSize: FontSize.medium.rawValue)
+//
+//        static let bigLight = FontHelper.light(withSize: FontSize.big.rawValue)
+//        static let bigBold = FontHelper.bold(withSize: FontSize.big.rawValue)
 
     }
 
