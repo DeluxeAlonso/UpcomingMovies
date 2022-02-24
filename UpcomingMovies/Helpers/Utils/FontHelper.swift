9b35216831a76c572a8d852726130c943ed53815
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-02-24 01:22:47 -0500

    Updates FontHelper class

--- UpcomingMovies/Helpers/Utils/FontHelper.swift
+++ UpcomingMovies/Helpers/Utils/FontHelper.swift
@@ -10,19 +10,19 @@ import UIKit
 
 struct FontHelper {
 
-    private static func bold(withSize size: CGFloat) -> UIFont {
+    static func bold(withSize size: CGFloat) -> UIFont {
         return UIFont.systemFont(ofSize: size, weight: UIFont.Weight.bold)
     }
 
-    private static func semiBold(withSize size: CGFloat) -> UIFont {
+    static func semiBold(withSize size: CGFloat) -> UIFont {
         return UIFont.systemFont(ofSize: size, weight: UIFont.Weight.semibold)
     }
 
-    private static func light(withSize size: CGFloat) -> UIFont {
+    static func light(withSize size: CGFloat) -> UIFont {
         return UIFont.systemFont(ofSize: size, weight: UIFont.Weight.light)
     }
 
-    private static func regular(withSize size: CGFloat) -> UIFont {
+    static func regular(withSize size: CGFloat) -> UIFont {
         return UIFont.systemFont(ofSize: size, weight: UIFont.Weight.regular)
     }
 
@@ -31,14 +31,25 @@ struct FontHelper {
 extension FontHelper {
 
     static let headline = FontHelper.semiBold(withSize: 21.0)
+    static let headlineBold = FontHelper.bold(withSize: 21.0)
+
     static let body = FontHelper.regular(withSize: 17.0)
+    static let bodySemiBold = FontHelper.semiBold(withSize: 17.0)
+    static let bodyBold = FontHelper.bold(withSize: 17.0)
+
     static let callout = FontHelper.regular(withSize: 16.0)
+    static let calloutLight = FontHelper.light(withSize: 16.0)
+    static let calloutSemiBold = FontHelper.semiBold(withSize: 16.0)
 
     static let subhead = FontHelper.regular(withSize: 15.0)
     static let subheadLight = FontHelper.light(withSize: 15.0)
+    static let subheadBold = FontHelper.bold(withSize: 15.0)
 
     static let footnote = FontHelper.regular(withSize: 13.0)
+    static let footnoteLight = FontHelper.light(withSize: 13.0)
+
     static let caption1 = FontHelper.regular(withSize: 12.0)
+    static let caption1Light = FontHelper.light(withSize: 12.0)
     static let caption2 = FontHelper.regular(withSize: 11.0)
 
 }
