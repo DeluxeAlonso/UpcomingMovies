9f22c1c72b33d0a83cb426ae368aefdc87ac4716
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-03-29 00:49:28 -0500

    Updates SearchMovies Assembly dependecies

--- UpcomingMovies/DependencyInjection/DependencyAssemblers/Scenes/Movies/SearchMoviesAssembly.swift
+++ UpcomingMovies/DependencyInjection/DependencyAssemblers/Scenes/Movies/SearchMoviesAssembly.swift
@@ -14,24 +14,33 @@ final class SearchMoviesAssembly: Assembly {
 
     func assemble(container: Container) {
         container.register(SearchOptionsInteractorProtocol.self) { resolver in
-            let useCaseProvider = resolver.resolve(UseCaseProviderProtocol.self)
-            return SearchOptionsInteractor(useCaseProvider: useCaseProvider!)
+            guard let useCaseProvider = resolver.resolve(UseCaseProviderProtocol.self) else {
+                fatalError("UseCaseProviderProtocol dependency could not be resolved")
+            }
+            return SearchOptionsInteractor(useCaseProvider: useCaseProvider)
         }
         container.register(SearchOptionsViewModelProtocol.self) { resolver in
-            let interactor = resolver.resolve(SearchOptionsInteractorProtocol.self)
-            return SearchOptionsViewModel(interactor: interactor!)
+            guard let interactor = resolver.resolve(SearchOptionsInteractorProtocol.self) else {
+                fatalError("SearchOptionsInteractorProtocol dependency could not be resolved")
+            }
+            return SearchOptionsViewModel(interactor: interactor)
         }
 
         container.register(SearchMoviesResultInteractorProtocol.self) { resolver in
-            let useCaseProvider = resolver.resolve(UseCaseProviderProtocol.self)
-            let authHandler = resolver.resolve(AuthenticationHandlerProtocol.self)
-
-            return SearchMoviesResultInteractor(useCaseProvider: useCaseProvider!,
-                                                authHandler: authHandler!)
+            guard let useCaseProvider = resolver.resolve(UseCaseProviderProtocol.self) else {
+                fatalError("UseCaseProviderProtocol dependency could not be resolved")
+            }
+            guard let authHandler = resolver.resolve(AuthenticationHandlerProtocol.self) else {
+                fatalError("AuthenticationHandlerProtocol dependency could not be resolved")
+            }
+            return SearchMoviesResultInteractor(useCaseProvider: useCaseProvider,
+                                                authHandler: authHandler)
         }
         container.register(SearchMoviesResultViewModelProtocol.self) { resolver in
-            let interactor = resolver.resolve(SearchMoviesResultInteractorProtocol.self)
-            return SearchMoviesResultViewModel(interactor: interactor!)
+            guard let interactor = resolver.resolve(SearchMoviesResultInteractorProtocol.self) else {
+                fatalError("SearchMoviesResultInteractorProtocol dependency could not be resolved")
+            }
+            return SearchMoviesResultViewModel(interactor: interactor)
         }
     }
 
