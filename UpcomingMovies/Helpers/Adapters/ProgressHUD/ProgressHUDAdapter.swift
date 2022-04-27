ad5bc788de5732638c813b6e15249b26e658e1c7
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-04-27 00:50:39 -0500

    Allows background user interaction in progress hud

--- UpcomingMovies/Helpers/Adapters/ProgressHUD/ProgressHUDAdapter.swift
+++ UpcomingMovies/Helpers/Adapters/ProgressHUD/ProgressHUDAdapter.swift
@@ -13,6 +13,7 @@ final class ProgressHUDAdapter: ProgressHUDAdapterProtocol {
     func showHUDWithOnlyText(_ text: String) {
         DLProgressHUD.defaultConfiguration.allowsDynamicTextWidth = true
         DLProgressHUD.defaultConfiguration.shouldDismissAutomatically = true
+        DLProgressHUD.defaultConfiguration.backgroundInteractionEnabled = true
         DLProgressHUD.defaultConfiguration.hudContentPreferredHeight = 64.0
         DLProgressHUD.show(.textOnly(text))
     }
