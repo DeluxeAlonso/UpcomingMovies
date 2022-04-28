fa963d2e51b1bd58f404c352cac4781a3135ed49
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-04-27 23:42:44 -0500

    Adds new method requirement in ProgressHUDAdapterProtocol

--- UpcomingMovies/Helpers/Adapters/ProgressHUD/ProgressHUDAdapterProtocol.swift
+++ UpcomingMovies/Helpers/Adapters/ProgressHUD/ProgressHUDAdapterProtocol.swift
@@ -6,8 +6,11 @@
 //  Copyright © 2022 Alonso. All rights reserved.
 //
 
+import UIKit
+
 protocol ProgressHUDAdapterProtocol {
 
     func showHUDWithOnlyText(_ text: String)
+    func showHUDWithOnlyText(_ text: String, in view: UIView)
 
 }
