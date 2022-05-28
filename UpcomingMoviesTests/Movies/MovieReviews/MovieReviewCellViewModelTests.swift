88c13d0560c5a6706683ca412a730a6093e89765
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-05-28 01:43:51 -0500

    Implements tests for MovieReviewCellViewModelTests

--- UpcomingMoviesTests/Movies/MovieReviews/MovieReviewCellViewModelTests.swift
+++ UpcomingMoviesTests/Movies/MovieReviews/MovieReviewCellViewModelTests.swift
@@ -7,27 +7,35 @@
 //
 
 import XCTest
+import UpcomingMoviesDomain
+@testable import UpcomingMovies
 
 class MovieReviewCellViewModelTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testMovieReviewCellAuthorName() {
+        // Arrange
+        let authorNametoTest = "Alonso"
+        let viewModel = createSUT(with: Review.with(authorName: authorNametoTest))
+        // Act
+        let viewModelAuthorName = viewModel.authorName
+        // Assert
+        XCTAssertEqual(viewModelAuthorName, authorNametoTest)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testMovieReviewCellContent() {
+        // Arrange
+        let contentToTest = "Review content"
+        let viewModel = createSUT(with: Review.with(content: contentToTest))
+        // Act
+        let viewModelContent = viewModel.content
+        // Assert
+        XCTAssertEqual(viewModelContent, contentToTest)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    // MARK: - Utils
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    private func createSUT(with review: Review) -> MovieReviewCellViewModel {
+        return MovieReviewCellViewModel(review)
     }
 
 }
