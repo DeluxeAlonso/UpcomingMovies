a90d803ed0c48a314c9fee1c3a22416ff269adff
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-06-01 23:57:12 -0500

    Adds unit tests for movie videos interactor

--- UpcomingMoviesTests/Movies/MovieVideos/MovieVideosInteractorTests.swift
+++ UpcomingMoviesTests/Movies/MovieVideos/MovieVideosInteractorTests.swift
@@ -7,27 +7,31 @@
 //
 
 import XCTest
+@testable import UpcomingMovies
+@testable import UpcomingMoviesDomain
 
 class MovieVideosInteractorTests: XCTestCase {
 
+    private var interactor: MovieVideosInteractor!
+    private var mockMovieUseCase: MovieUseCaseProtocolMock!
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        mockMovieUseCase = MovieUseCaseProtocolMock()
+        interactor = MovieVideosInteractor(movieUseCase: mockMovieUseCase)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        mockMovieUseCase = nil
+        interactor = nil
+        try tearDownWithError()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testGetMovieVideosCalled() {
+        // Act
+        interactor.getMovieVideos(for: 0, page: 1, completion: { _ in })
+        // Assert
+        XCTAssertTrue(mockMovieUseCase.getMovieVideosCalled)
     }
 
 }
