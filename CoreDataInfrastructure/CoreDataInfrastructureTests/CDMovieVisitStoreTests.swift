fea5f059d3d1fc528ba7bd6bf7e89362cf50c996
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-06-11 14:24:17 -0500

    Deletes unused files in CoreDataInfrastructureTests

--- CoreDataInfrastructure/CoreDataInfrastructureTests/CDMovieVisitStoreTests.swift
+++ CoreDataInfrastructure/CoreDataInfrastructureTests/CDMovieVisitStoreTests.swift
@@ -8,31 +8,6 @@
 
 import XCTest
 
-@testable import CoreDataInfrastructure
-@testable import UpcomingMoviesDomain
-import CoreData
-
-protocol CDStoreTestsProtocol {
-    associatedtype CDEntity: NSManagedObject & Managed
-
-    var storeToTest: PersistenceStore<CDEntity>! { get }
-    var mockPersistantContainer: NSPersistentContainer! { get }
-
-    func flush() throws
-}
-
-extension CDStoreTestsProtocol {
-    func flush() throws {
-        guard let context = mockPersistantContainer?.viewContext else { return }
-        let fetchRequest:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest<NSFetchRequestResult>(entityName: CDEntity.entityName)
-        let objs = try context.fetch(fetchRequest)
-        for case let obj as NSManagedObject in objs {
-            context.delete(obj)
-        }
-        try context.save()
-    }
-}
-
 class CDMovieVisitStoreTests: XCTestCase, CDStoreTestsProtocol {
 
     var storeToTest: PersistenceStore<CDMovieVisit>!
