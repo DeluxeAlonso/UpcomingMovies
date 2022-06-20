2c92078083b8eb31ee9f08f616b365ca72ef3cab
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-06-19 23:20:24 -0500

    Adds unit tests for movie visit repository

--- UpcomingMoviesData/UpcomingMoviesDataTests/MovieVisitRepositoryTests.swift
+++ UpcomingMoviesData/UpcomingMoviesDataTests/MovieVisitRepositoryTests.swift
@@ -12,26 +12,55 @@ import XCTest
 class MovieVisitRepositoryTests: XCTestCase {
 
     private var repository: MovieVisitRepository!
-    private var mockMovieVisitLocalDataSource: MovieVisitLocalDataSourceProtocolMock!
+    private var movieVisitLocalDataSource: MovieVisitLocalDataSourceProtocolMock!
 
     override func setUpWithError() throws {
         try super.setUpWithError()
-        mockMovieVisitLocalDataSource = MovieVisitLocalDataSourceProtocolMock()
-        repository = MovieVisitRepository(localDataSource: mockMovieVisitLocalDataSource)
+        movieVisitLocalDataSource = MovieVisitLocalDataSourceProtocolMock()
+        repository = MovieVisitRepository(localDataSource: movieVisitLocalDataSource)
     }
 
     override func tearDownWithError() throws {
         repository = nil
-        mockMovieVisitLocalDataSource = nil
+        movieVisitLocalDataSource = nil
         try super.tearDownWithError()
     }
 
     func testGetMovieVisits() {
         // Arrange
-        let movieVisitsToTest: [UpcomingMoviesDomain.MovieVisit] = []
+        let movieVisitsToTest: [UpcomingMoviesDomain.MovieVisit] = [MovieVisit.with()]
         let expectation = XCTestExpectation(description: "Should get movie visits")
+        // Act
+        movieVisitLocalDataSource.getMovieVisitsResult = Result.success(movieVisitsToTest)
+        repository.getMovieVisits { result in
+            guard let movieVisits = try? result.get() else {
+                XCTFail()
+                return
+            }
+            XCTAssertEqual(movieVisits, movieVisitsToTest)
+            expectation.fulfill()
+        }
+        // Assert
+        wait(for: [expectation], timeout: 1.0)
+        XCTAssertEqual(movieVisitLocalDataSource.getMovieVisitsCallCount, 1)
+    }
 
-        
+    func testSaveMovieVisits() {
+        // Arrange
+        let expectation = XCTestExpectation(description: "Should get a success save result")
+        // Act
+        movieVisitLocalDataSource.saveResult = Result.success(())
+        repository.save(with: 1, title: "", posterPath: "") { result in
+            switch result {
+            case .success:
+                expectation.fulfill()
+            case .failure:
+                XCTFail()
+            }
+        }
+        // Assert
+        wait(for: [expectation], timeout: 1.0)
+        XCTAssertEqual(self.movieVisitLocalDataSource.saveCallCount, 1)
     }
 
 }
