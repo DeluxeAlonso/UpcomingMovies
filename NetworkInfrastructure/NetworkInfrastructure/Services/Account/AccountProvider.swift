8a8f1ce6664cc5915033850731d0c74e1b2f0dfc
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-08-24 00:08:57 -0500

    Adds sort_by parameter to favorites endpoint

--- NetworkInfrastructure/NetworkInfrastructure/Services/Account/AccountProvider.swift
+++ NetworkInfrastructure/NetworkInfrastructure/Services/Account/AccountProvider.swift
@@ -66,7 +66,9 @@ extension AccountProvider: Endpoint {
         case .getAccountDetail(let sessionId):
             return ["session_id": sessionId]
         case .getFavoriteList(let page, let sessionId, _):
-            return ["session_id": sessionId, "page": page]
+            return ["session_id": sessionId,
+                    "page": page,
+                    "sort_by": "created_at.desc"]
         case .getWatchlist(let page, let sessionId, _):
             return ["session_id": sessionId,
                     "page": page,
