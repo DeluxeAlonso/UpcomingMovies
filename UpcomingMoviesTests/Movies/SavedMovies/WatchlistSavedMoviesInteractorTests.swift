90dfcd726231f75d56aafaa83abd3aeb7947ef79
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-08-27 01:12:24 -0500

    Implements WatchlistSavedMoviesInteractorTests

--- UpcomingMoviesTests/Movies/SavedMovies/WatchlistSavedMoviesInteractorTests.swift
+++ UpcomingMoviesTests/Movies/SavedMovies/WatchlistSavedMoviesInteractorTests.swift
@@ -7,27 +7,39 @@
 //
 
 import XCTest
+@testable import UpcomingMovies
+@testable import UpcomingMoviesDomain
 
 class WatchlistSavedMoviesInteractorTests: XCTestCase {
 
+    private var interactor: WatchlistSavedMoviesInteractor!
+    private var accountUseCase: AccountUseCaseProtocolMock! = AccountUseCaseProtocolMock()
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        interactor = WatchlistSavedMoviesInteractor(accountUseCase: accountUseCase)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        interactor = nil
+        accountUseCase = nil
+        try super.tearDownWithError()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testGetWatchlistCalled() {
+        // Arrange
+        let moviesToTest = [Movie.with(id: 1)]
+        accountUseCase.getWatchlistResult = .success(moviesToTest)
+        // Act
+        interactor.getSavedMovies(page: 1) { movies in
+            guard let movies = try? movies.get() else {
+                XCTFail("No valid movies")
+                return
+            }
+            XCTAssertEqual(movies, moviesToTest)
         }
+        // Assert
+        XCTAssertEqual(accountUseCase.getWatchlistCallCount, 1)
     }
 
 }
