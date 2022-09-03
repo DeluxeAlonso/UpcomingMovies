e0e257146867d1deed05d0c2a11e79c619c3d0c6
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-09-03 00:10:34 -0500

    Implements AuthenticationManagerProtocolMock

--- NetworkInfrastructure/NetworkInfrastructure/TestDoubles/AuthenticationManagerProtocolMock.swift
+++ NetworkInfrastructure/NetworkInfrastructure/TestDoubles/AuthenticationManagerProtocolMock.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+final class AuthenticationManagerProtocolMock: AuthenticationManagerProtocol {
+
+    var readAccessToken: String = ""
+    var accessToken: AccessToken?
+    var userAccount: Account?
+    var requestToken: String?
+
+    private (set) var saveCurrentUserCallCount = 0
+    func saveCurrentUser(_ sessionId: String, accountId: Int) {
+        saveCurrentUserCallCount += 1
+    }
+
+    private (set) var deleteCurrentUserCallCount = 0
+    func deleteCurrentUser() {
+        deleteCurrentUserCallCount += 1
+    }
+
+    private (set) var ssaveRequestTokenCallCount = 0
+    func saveRequestToken(_ requestToken: String) {
+        ssaveRequestTokenCallCount += 1
+    }
+
+    private (set) var saveAccessTokenCallCount = 0
+    func saveAccessToken(_ accessToken: AccessToken) {
+        saveAccessTokenCallCount += 1
+    }
+
+}
