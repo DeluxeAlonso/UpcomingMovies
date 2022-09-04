5b2dc7f311d8651c4737eeda16c4149943befce6
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-09-03 19:17:54 -0500

    Adds unit tests for favorite list in account remote data source

--- NetworkInfrastructure/NetworkInfrastructure/Core/Entities/Movie.swift
+++ NetworkInfrastructure/NetworkInfrastructure/Core/Entities/Movie.swift
@@ -60,13 +60,13 @@ extension Movie: DomainConvertible {
 
 extension Movie {
 
-    static func with(id: Int = 1,
-                     title: String = "Movie 1",
-                     genreIds: [Int] = [],
-                     overview: String = "Overview",
-                     posterPath: String? = "/poster.jpg",
-                     backdropPath: String? = "/backdrop.jpg",
-                     releaseDate: String = "02-21-2019", voteAverage: Double = 5.0) -> Movie {
+    static func create(id: Int = 1,
+                       title: String = "Movie 1",
+                       genreIds: [Int] = [],
+                       overview: String = "Overview",
+                       posterPath: String? = "/poster.jpg",
+                       backdropPath: String? = "/backdrop.jpg",
+                       releaseDate: String = "02-21-2019", voteAverage: Double = 5.0) -> Movie {
         return Movie(id: id, title: title, genreIds: genreIds,
                      overview: overview, posterPath: posterPath,
                      backdropPath: backdropPath, releaseDate: releaseDate,
