f23dba528449dc496ab1d5edd48a1ce8cf582419
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-09-03 22:28:09 -0500

    Deletes autogenerated code

--- NetworkInfrastructure/NetworkInfrastructureTests/AccountRemoteDataSourceTests.swift
+++ NetworkInfrastructure/NetworkInfrastructureTests/AccountRemoteDataSourceTests.swift
@@ -78,11 +78,4 @@ class AccountRemoteDataSourceTests: XCTestCase {
         XCTAssertEqual(accountClient.getFavoriteListCallCount, 0)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
