0db4db2a3ac3cdb8871f4c3df1819b8a7913fde0
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-09-09 23:58:16 -0500

    Moves movie mock initializer to test doubles group

---
+++ NetworkInfrastructure/NetworkInfrastructure/TestDoubles/Entities/Movie+MockInitializer.swift
@@ -0,0 +1,25 @@
+//
+//  Movie+MockInitializer.swift
+//  NetworkInfrastructure
+//
+//  Created by Alonso on 9/09/22.
+//
+
+import Foundation
+
+extension Movie {
+
+    static func create(id: Int = 1,
+                       title: String = "Movie 1",
+                       genreIds: [Int] = [],
+                       overview: String = "Overview",
+                       posterPath: String? = "/poster.jpg",
+                       backdropPath: String? = "/backdrop.jpg",
+                       releaseDate: String = "02-21-2019", voteAverage: Double = 5.0) -> Movie {
+        return Movie(id: id, title: title, genreIds: genreIds,
+                     overview: overview, posterPath: posterPath,
+                     backdropPath: backdropPath, releaseDate: releaseDate,
+                     voteAverage: voteAverage)
+    }
+
+}
