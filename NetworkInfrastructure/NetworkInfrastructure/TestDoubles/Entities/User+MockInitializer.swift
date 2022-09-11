6e0a1027e20a4714e57afa72949d53453d69b1f3
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-09-11 02:09:36 -0500

    Creates user mock initializer

---
+++ NetworkInfrastructure/NetworkInfrastructure/TestDoubles/Entities/User+MockInitializer.swift
@@ -0,0 +1,17 @@
+//
+//  User+MockInitializer.swift
+//  NetworkInfrastructure
+//
+//  Created by Alonso on 11/09/22.
+//
+
+extension User {
+
+    static func create(id: Int = 1,
+                       name: String = "Test",
+                       username: String = "Username",
+                       includeAdult: Bool = false) -> User {
+        return User(id: id, name: name, username: username, includeAdult: includeAdult)
+    }
+
+}
