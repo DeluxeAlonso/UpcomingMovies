0d7dcc7ce5132c9747dac763a155aa8a1fa05c0c
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-11-23 23:49:00 -0500

    Implements AnyPublishBindable class

---
+++ UpcomingMovies/Helpers/Utils/Bindable/PublishBindable/AnyPublishBindable.swift
@@ -0,0 +1,32 @@
+//
+//  AnyPublishBindable.swift
+//  UpcomingMovies
+//
+//  Created by Alonso on 23/11/22.
+//  Copyright © 2022 Alonso. All rights reserved.
+//
+
+import Dispatch
+
+final class AnyPublishBindable<T>: PublishBindableProtocol {
+
+    typealias Model = T
+    typealias Listener = ((T) -> Void)
+
+    private let sendClosure: ((T) -> Void)
+    private let bindClosure: ((@escaping Listener, DispatchQueue?) -> Void)
+
+    init<B: PublishBindableProtocol>(_ bindable: B) where B.Model == T {
+        self.sendClosure = bindable.send
+        self.bindClosure = bindable.bind
+    }
+
+    func bind(_ listener: @escaping Listener, on dispatchQueue: DispatchQueue?) {
+        bindClosure(listener, dispatchQueue)
+    }
+
+    func send(_ value: T) {
+        sendClosure(value)
+    }
+
+}
