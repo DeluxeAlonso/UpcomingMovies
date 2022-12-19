03233a4b37bc6de58b5a227764790d81a9c6e3a3
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-12-19 00:11:03 -0500

    Makes SavedMoviesCoordinator inherit from BaseCoordinator class

--- UpcomingMovies/Scenes/Account/Saved Movies/SavedMoviesCoordinator.swift
+++ UpcomingMovies/Scenes/Account/Saved Movies/SavedMoviesCoordinator.swift
@@ -8,38 +8,25 @@
 
 import UIKit
 
-final class FavoritesSavedMoviesCoordinator: SavedMoviesCoordinatorProtocol, Coordinator, MovieDetailCoordinable {
+final class FavoritesSavedMoviesCoordinator: BaseCoordinator, SavedMoviesCoordinatorProtocol, MovieDetailCoordinable {
 
-    var childCoordinators: [Coordinator] = []
-    var parentCoordinator: Coordinator?
-    var navigationController: UINavigationController
-
-    init(navigationController: UINavigationController) {
-        self.navigationController = navigationController
-    }
-
-    func start() {
+    override func start() {
         let viewController = SavedMoviesViewController.instantiate()
 
         viewController.viewModel = DIContainer.shared.resolve(argument: ProfileOption.favorites.title)
         viewController.coordinator = self
 
+        if navigationController.delegate == nil {
+            navigationController.delegate = self
+        }
         navigationController.pushViewController(viewController, animated: true)
     }
 
 }
 
-final class WatchlistSavedMoviesCoordinator: SavedMoviesCoordinatorProtocol, Coordinator, MovieDetailCoordinable {
+final class WatchlistSavedMoviesCoordinator: BaseCoordinator, SavedMoviesCoordinatorProtocol, MovieDetailCoordinable {
 
-    var childCoordinators: [Coordinator] = []
-    var parentCoordinator: Coordinator?
-    var navigationController: UINavigationController
-
-    init(navigationController: UINavigationController) {
-        self.navigationController = navigationController
-    }
-
-    func start() {
+    override func start() {
         let viewController = SavedMoviesViewController.instantiate()
 
         viewController.viewModel = DIContainer.shared.resolve(argument: ProfileOption.watchlist.title)
