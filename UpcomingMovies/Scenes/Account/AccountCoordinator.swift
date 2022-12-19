2d1367cd15a32af5859e7d0074e741871dcd759e
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-12-18 23:51:40 -0500

    Makes AccountCoordinator inherit from BaseCoordinator class

--- UpcomingMovies/Scenes/Account/AccountCoordinator.swift
+++ UpcomingMovies/Scenes/Account/AccountCoordinator.swift
@@ -9,17 +9,7 @@
 import UIKit
 import UpcomingMoviesDomain
 
-final class AccountCoordinator: NSObject, AccountCoordinatorProtocol, RootCoordinator {
-
-    var childCoordinators: [Coordinator] = []
-    var parentCoordinator: Coordinator?
-    var navigationController: UINavigationController
-
-    // MARK: - Initializers
-
-    init(navigationController: UINavigationController) {
-        self.navigationController = navigationController
-    }
+final class AccountCoordinator: BaseCoordinator, AccountCoordinatorProtocol, RootCoordinator {
 
     // MARK: - Coordinator
 
@@ -27,13 +17,15 @@ final class AccountCoordinator: NSObject, AccountCoordinatorProtocol, RootCoordi
         return RootCoordinatorIdentifier.account
     }
 
-    func start() {
+    override func start() {
         let viewController = AccountViewController.instantiate()
 
         viewController.viewModel = DIContainer.shared.resolve()
         viewController.coordinator = self
 
-        navigationController.delegate = self
+        if navigationController.delegate == nil {
+            navigationController.delegate = self
+        }
         navigationController.pushViewController(viewController, animated: true)
     }
 
@@ -140,21 +132,3 @@ final class AccountCoordinator: NSObject, AccountCoordinatorProtocol, RootCoordi
     }
 
 }
-
-// MARK: - UINavigationControllerDelegate
-
- extension AccountCoordinator: UINavigationControllerDelegate {
-
-     func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
-         guard let fromViewController = navigationController.transitionCoordinator?.viewController(forKey: .from) else {
-             return
-         }
-         // Check whether our view controller array already contains that view controller.
-         // If it does it means we’re pushing a different view controller on top rather than popping it, so exit.
-         if navigationController.viewControllers.contains(fromViewController) {
-             return
-         }
-         unwrappedParentCoordinator.childDidFinish()
-     }
-
- }
