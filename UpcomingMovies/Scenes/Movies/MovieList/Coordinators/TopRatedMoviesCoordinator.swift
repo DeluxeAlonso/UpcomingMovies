3e4f810fbdf68ac420aa363ef7a435b7860f57d3
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-12-20 01:36:40 -0500

    Makes TopRatedMoviesCoordinator inherit from BaseCoordinator class

--- UpcomingMovies/Scenes/Movies/MovieList/Coordinators/TopRatedMoviesCoordinator.swift
+++ UpcomingMovies/Scenes/Movies/MovieList/Coordinators/TopRatedMoviesCoordinator.swift
@@ -9,23 +9,18 @@
 import UIKit
 import UpcomingMoviesDomain
 
-final class TopRatedMoviesCoordinator: MovieListCoordinatorProtocol, Coordinator, MovieDetailCoordinable {
+final class TopRatedMoviesCoordinator: BaseCoordinator, MovieListCoordinatorProtocol, MovieDetailCoordinable {
 
-    var childCoordinators: [Coordinator] = []
-    var parentCoordinator: Coordinator?
-    var navigationController: UINavigationController
-
-    init(navigationController: UINavigationController) {
-        self.navigationController = navigationController
-    }
-
-    func start() {
+    override func start() {
         let viewController = MovieListViewController.instantiate()
 
         viewController.viewModel = DIContainer.shared.resolve(name: "TopRatedMovies",
                                                               argument: LocalizedStrings.topRatedMoviesTitle())
         viewController.coordinator = self
 
+        if navigationController.delegate == nil {
+            navigationController.delegate = self
+        }
         navigationController.pushViewController(viewController, animated: true)
     }
 
