8c311f677ea71307ce58492dd8bdca2ddc88b1b4
DeluxeAlonso <alonso.alvarez@pucp.pe>
2022-12-23 21:17:06 -0500

    Makes MovieReviewsCoordinator adopt BaseCoordinator class

--- UpcomingMovies/Scenes/Movies/MovieReviews/MovieReviewsCoordinator.swift
+++ UpcomingMovies/Scenes/Movies/MovieReviews/MovieReviewsCoordinator.swift
@@ -9,25 +9,24 @@
 import UIKit
 import UpcomingMoviesDomain
 
-final class MovieReviewsCoordinator: Coordinator, MovieReviewsCoordinatorProtocol {
-
-    var childCoordinators: [Coordinator] = []
-    var parentCoordinator: Coordinator?
-    var navigationController: UINavigationController
+final class MovieReviewsCoordinator: BaseCoordinator, MovieReviewsCoordinatorProtocol {
 
     var movieId: Int!
     var movieTitle: String!
 
-    init(navigationController: UINavigationController) {
-        self.navigationController = navigationController
-    }
+//    init(navigationController: UINavigationController) {
+//        self.navigationController = navigationController
+//    }
 
-    func start() {
+    override func start() {
         let viewController = MovieReviewsViewController.instantiate()
 
         viewController.viewModel = DIContainer.shared.resolve(arguments: movieId, movieTitle)
         viewController.coordinator = self
 
+        if navigationController.delegate == nil {
+            navigationController.delegate = self
+        }
         navigationController.pushViewController(viewController, animated: true)
     }
 
