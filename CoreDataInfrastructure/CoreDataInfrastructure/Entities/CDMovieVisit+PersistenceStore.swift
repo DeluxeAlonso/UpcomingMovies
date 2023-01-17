5a29ce8fdd28dc2b9af2a5e62526b30616ee598c
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-01-16 21:51:17 -0500

    Updates findAll method in movie visit persistent store

--- CoreDataInfrastructure/CoreDataInfrastructure/Entities/CDMovieVisit+PersistenceStore.swift
+++ CoreDataInfrastructure/CoreDataInfrastructure/Entities/CDMovieVisit+PersistenceStore.swift
@@ -29,7 +29,7 @@ extension PersistenceStore where Entity == CDMovieVisit {
     func findAll(completion: (([CDMovieVisit]) -> Void)? = nil) {
         let context = createBackgroundContext()
         context.automaticallyMergesChangesFromParent = true
-        context.performAndWait {
+        context.perform {
             let movieVisits = CDMovieVisit.fetch(in: context)
             completion?(movieVisits)
         }
