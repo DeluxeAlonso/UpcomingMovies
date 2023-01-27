106c3c2d8d9822b66c8dc50912acb343628fac88
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-01-26 23:47:02 -0500

    Fixes syntax issues in AuthenticationManagerProtocolMock

--- NetworkInfrastructure/NetworkInfrastructure/TestDoubles/AuthenticationManagerProtocolMock.swift
+++ NetworkInfrastructure/NetworkInfrastructure/TestDoubles/AuthenticationManagerProtocolMock.swift
@@ -12,22 +12,22 @@ final class AuthenticationManagerProtocolMock: AuthenticationManagerProtocol {
     var userAccount: Account?
     var requestToken: String?
 
-    private (set) var saveCurrentUserCallCount = 0
+    private(set) var saveCurrentUserCallCount = 0
     func saveCurrentUser(_ sessionId: String, accountId: Int) {
         saveCurrentUserCallCount += 1
     }
 
-    private (set) var deleteCurrentUserCallCount = 0
+    private(set) var deleteCurrentUserCallCount = 0
     func deleteCurrentUser() {
         deleteCurrentUserCallCount += 1
     }
 
-    private (set) var ssaveRequestTokenCallCount = 0
+    private(set) var ssaveRequestTokenCallCount = 0
     func saveRequestToken(_ requestToken: String) {
         ssaveRequestTokenCallCount += 1
     }
 
-    private (set) var saveAccessTokenCallCount = 0
+    private(set) var saveAccessTokenCallCount = 0
     func saveAccessToken(_ accessToken: AccessToken) {
         saveAccessTokenCallCount += 1
     }
