d79620d0ab562d78003ce403f2e85b78d42fef64
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-02-07 21:15:54 -0500

    Fixes minor swiftlint warnings

--- UpcomingMovies/Helpers/Utils/UserDefaultsStorage.swift
+++ UpcomingMovies/Helpers/Utils/UserDefaultsStorage.swift
@@ -21,7 +21,7 @@ struct UserDefaultsStorage<T> {
 
     var wrappedValue: T {
         get {
-            return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+            UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
         }
         set {
             UserDefaults.standard.set(newValue, forKey: key)
