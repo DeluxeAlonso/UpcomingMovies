12463793c2ec943c6b7f866accf0b9f138fa87ce
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-02-07 21:15:36 -0500

    Implements UserDefaultsStorage

--- UpcomingMovies/Helpers/Utils/UserDefaultsStorage.swift
+++ UpcomingMovies/Helpers/Utils/UserDefaultsStorage.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+
+@propertyWrapper
+struct UserDefaultsStorage<T> {
+
+    private let key: String
+    private let defaultValue: T
+
+    init(key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+
+    var wrappedValue: T {
+        get {
+            return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: key)
+        }
+    }
+
+}
