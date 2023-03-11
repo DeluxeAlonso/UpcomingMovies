34592313f20134bbaf33904b8dcebaf4f7aa17a2
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-03-10 23:48:51 -0500

    Fix minor syntax issue in SearchMoviesResultController

--- UpcomingMovies/Scenes/Movies/SearchMovies/SearchResult/SearchMoviesResultController.swift
+++ UpcomingMovies/Scenes/Movies/SearchMovies/SearchResult/SearchMoviesResultController.swift
@@ -111,7 +111,7 @@ class SearchMoviesResultController: UIViewController, Keyboardable {
 
     private func setupBindables() {
         viewModel.viewState.bindAndFire({ [weak self] state in
-            guard let self = self else { return }
+            guard let self else { return }
             self.configureView(with: state)
             self.reloadTableView()
         }, on: .main)
