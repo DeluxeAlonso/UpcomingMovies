aee60f0bfc9019d019e823e62b51ce270b9054ac
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-03-28 23:54:39 -0500

    Adds minor updates to MockMovieDetailInteractor

--- UpcomingMoviesTests/Helpers/Mocks/MovieDetailMockFactory.swift
+++ UpcomingMoviesTests/Helpers/Mocks/MovieDetailMockFactory.swift
@@ -11,39 +11,51 @@
 
 class MockMovieDetailInteractor: MovieDetailInteractorProtocol {
 
-    var isUserSignedInResult: Bool? = false
+    var isUserSignedInResult: Bool = false
     func isUserSignedIn() -> Bool {
-        isUserSignedInResult!
+        isUserSignedInResult
     }
 
     var findGenreResult: Result<Genre?, Error>? = .success(nil)
     func findGenre(with id: Int, completion: @escaping (Result<Genre?, Error>) -> Void) {
-        completion(findGenreResult!)
+        if let findGenreResult {
+            completion(findGenreResult)
+        }
     }
 
     var getMovieDetailResult: Result<Movie, Error>?
     func getMovieDetail(for movieId: Int, completion: @escaping (Result<Movie, Error>) -> Void) {
-        completion(getMovieDetailResult!)
+        if let getMovieDetailResult {
+            completion(getMovieDetailResult)
+        }
     }
 
     var getMovieAccountStateResult: Result<Movie.AccountState, Error>?
     func getMovieAccountState(for movieId: Int, completion: @escaping (Result<Movie.AccountState, Error>) -> Void) {
-        completion(getMovieAccountStateResult!)
+        if let getMovieAccountStateResult {
+            completion(getMovieAccountStateResult)
+        }
     }
 
     var markMovieAsFavoriteResult: Result<Bool, Error>?
     func markMovieAsFavorite(movieId: Int, favorite: Bool, completion: @escaping (Result<Bool, Error>) -> Void) {
-        completion(markMovieAsFavoriteResult!)
+        if let markMovieAsFavoriteResult {
+            completion(markMovieAsFavoriteResult)
+        }
     }
 
     var addToWatchlistResult: Result<Bool, Error>?
     func addToWatchlist(movieId: Int, completion: @escaping (Result<Bool, Error>) -> Void) {
-        completion(addToWatchlistResult!)
+        if let addToWatchlistResult {
+            completion(addToWatchlistResult)
+        }
     }
 
     var removeFromWatchlistResult: Result<Bool, Error>?
     func removeFromWatchlist(movieId: Int, completion: @escaping (Result<Bool, Error>) -> Void) {
-        completion(removeFromWatchlistResult!)
+        if let removeFromWatchlistResult {
+            completion(removeFromWatchlistResult)
+        }
     }
 
     func saveMovieVisit(with id: Int, title: String, posterPath: String?) {}
