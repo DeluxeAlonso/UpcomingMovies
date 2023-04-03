6944b2003047137cba8b93ab082077e3f785b6b7
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-04-03 00:06:57 -0500

    Implements UpcomingMoviesLayoutProvider

--- UpcomingMovies/Scenes/Movies/UpcomingMovies/UpcomingMoviesLayoutProvider.swift
+++ UpcomingMovies/Scenes/Movies/UpcomingMovies/UpcomingMoviesLayoutProvider.swift
@@ -6,12 +6,39 @@
 //  Copyright © 2023 Alonso. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 protocol UpcomingMoviesLayoutProviderProtocol {
+
+    func collectionViewLayout(for presentationMode: UpcomingMoviesPresentationMode,
+                              and width: CGFloat) -> UICollectionViewLayout
 
 }
 
 struct UpcomingMoviesLayoutProvider: UpcomingMoviesLayoutProviderProtocol {
+
+    func collectionViewLayout(for presentationMode: UpcomingMoviesPresentationMode, and width: CGFloat) -> UICollectionViewLayout {
+        switch presentationMode {
+        case .detail:
+            let detailLayoutWidth = width - Constants.detailCellOffset
+            return VerticalFlowLayout(preferredWidth: detailLayoutWidth, preferredHeight: Constants.detailCellHeight)
+        case .preview:
+            let previewLayoutWidth = Constants.previewCellHeight / CGFloat(UIConstants.posterAspectRatio)
+            return VerticalFlowLayout(preferredWidth: previewLayoutWidth,
+                                      preferredHeight: Constants.previewCellHeight,
+                                      minColumns: Constants.previewLayoutMinColumns)
+        }
+    }
+
+    struct Constants {
+
+        static let previewCellHeight: CGFloat = 150.0
+
+        static let detailCellHeight: CGFloat = 200.0
+        static let detailCellOffset: CGFloat = 32.0
+
+        static let previewLayoutMinColumns: Int = 3
+
+    }
 
 }
