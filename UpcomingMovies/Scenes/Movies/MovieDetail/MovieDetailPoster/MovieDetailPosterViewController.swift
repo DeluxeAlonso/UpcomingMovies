13aa2ff3d8baf810e656b942cfbaaa44528ba8a3
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-04-18 23:17:05 -0500

    Creates movie detail poster view controller

---
+++ UpcomingMovies/Scenes/Movies/MovieDetail/MovieDetailPoster/MovieDetailPosterViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MovieDetailPosterViewController.swift
+//  UpcomingMovies
+//
+//  Created by Alonso on 18/04/23.
+//  Copyright © 2023 Alonso. All rights reserved.
+//
+
+import UIKit
+
+final class MovieDetailPosterViewController: UIViewController, Storyboarded {
+
+    static var storyboardName: String = "MovieDetail"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
