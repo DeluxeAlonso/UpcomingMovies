cb45c0023156b4557451c9c34f05bb9643a9ba08
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-06-01 00:37:38 -0500

    Adds minor updates to MovieDetailOptionView

--- UpcomingMovies/Scenes/Movies/MovieDetail/MovieDetailOptions/MovieDetailOptionView.swift
+++ UpcomingMovies/Scenes/Movies/MovieDetail/MovieDetailOptions/MovieDetailOptionView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MovieDetailOptionView: UIView {
+final class MovieDetailOptionView: UIView {
 
     private lazy var optionStackView: UIStackView = {
         let stackView = UIStackView()
