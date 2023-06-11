64251be634c868b03cff16842dc22f247cf0bb1d
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-06-11 12:21:59 -0500

    Adds unit tests for search movies coordinator

--- UpcomingMoviesTests/Movies/SearchMovies/SearchMoviesCoordinatorTests.swift
+++ UpcomingMoviesTests/Movies/SearchMovies/SearchMoviesCoordinatorTests.swift
@@ -24,6 +24,15 @@ final class SearchMoviesCoordinatorTests: XCTestCase {
         try super.tearDownWithError()
     }
 
+    func testRootIdentifier() {
+        // Arrange
+        let coordinator = createSUT()
+        // Act
+        let rootIdentifier = coordinator.rootIdentifier
+        // Assert
+        XCTAssertEqual(rootIdentifier, RootCoordinatorIdentifier.searchMovies)
+    }
+
     func testStart() {
         // Arrange
         let coordinator = createSUT()
@@ -43,6 +52,78 @@ final class SearchMoviesCoordinatorTests: XCTestCase {
         _ = coordinator.embedSearchOptions(on: parentViewController, in: containerView)
         // Assert
         XCTAssertEqual(parentViewController.addChildCallCount, 1)
+    }
+
+    func testEmbedSearchController() {
+        // Arrange
+        let coordinator = createSUT()
+        let parentViewController = MockSearchMoviesResultControllerDelegate()
+        let containerView = UIView()
+        parentViewController.view.addSubview(containerView)
+        // Act
+        let searchController = coordinator.embedSearchController(on: parentViewController)
+        guard let searchResultsController = searchController.searchResultsController as? SearchMoviesResultController else {
+            XCTFail("Invalid search results controller")
+            return
+        }
+        // Assert
+        XCTAssertNotNil(searchResultsController.delegate)
+    }
+
+    func testShowMovieDetail() {
+        // Arrange
+        let coordinator = createSUT()
+        // Act
+        coordinator.showMovieDetail(for: 1, and: "Title")
+        // Assert
+        XCTAssertEqual(coordinator.unwrappedParentCoordinator.childCoordinators.count, 1)
+        guard let childCoordinator = coordinator.unwrappedParentCoordinator.childCoordinators.first else {
+            XCTFail("No child coordinator available")
+            return
+        }
+        XCTAssertTrue(childCoordinator is MovieDetailCoordinatorProtocol)
+    }
+
+    func testShowMoviesByGenre() {
+        // Arrange
+        let coordinator = createSUT()
+        // Act
+        coordinator.showMoviesByGenre(1, genreName: "Genre Name")
+        // Assert
+        XCTAssertEqual(coordinator.unwrappedParentCoordinator.childCoordinators.count, 1)
+        guard let childCoordinator = coordinator.unwrappedParentCoordinator.childCoordinators.first else {
+            XCTFail("No child coordinator available")
+            return
+        }
+        XCTAssertTrue(childCoordinator is MoviesByGenreCoordinator)
+    }
+
+    func testShowPopularMovies() {
+        // Arrange
+        let coordinator = createSUT()
+        // Act
+        coordinator.showDefaultSearchOption(.popular)
+        // Assert
+        XCTAssertEqual(coordinator.unwrappedParentCoordinator.childCoordinators.count, 1)
+        guard let childCoordinator = coordinator.unwrappedParentCoordinator.childCoordinators.first else {
+            XCTFail("No child coordinator available")
+            return
+        }
+        XCTAssertTrue(childCoordinator is PopularMoviesCoordinator)
+    }
+
+    func testShowTopRatedMovies() {
+        // Arrange
+        let coordinator = createSUT()
+        // Act
+        coordinator.showDefaultSearchOption(.topRated)
+        // Assert
+        XCTAssertEqual(coordinator.unwrappedParentCoordinator.childCoordinators.count, 1)
+        guard let childCoordinator = coordinator.unwrappedParentCoordinator.childCoordinators.first else {
+            XCTFail("No child coordinator available")
+            return
+        }
+        XCTAssertTrue(childCoordinator is TopRatedMoviesCoordinator)
     }
 
     private func createSUT() -> SearchMoviesCoordinator {
