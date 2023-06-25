d81a1391bd6d909022413e44b2a7617c8155e91c
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-06-25 13:11:36 -0500

    Updates unit tests for MovieReviewDetailCoordinator

--- UpcomingMovies/Scenes/Movies/MovieReviewDetail/MovieReviewDetailCoordinator.swift
+++ UpcomingMovies/Scenes/Movies/MovieReviewDetail/MovieReviewDetailCoordinator.swift
@@ -21,43 +21,13 @@ final class MovieReviewDetailCoordinator: BaseCoordinator, MovieReviewDetailCoor
         super.init(navigationController: navigationController)
     }
 
-    override func build() -> UIViewController {
+    override func build() -> MovieReviewDetailViewController {
         let viewController = MovieReviewDetailViewController.instantiate()
 
         viewController.viewModel = DIContainer.shared.resolve(argument: review)
         viewController.coordinator = self
 
         return viewController
-    }
-
-//    override func start() {
-//        let viewController = build()
-//
-//        navigationController.pushViewController(viewController, animated: false)
-//        navigationController.modalPresentationStyle = .fullScreen
-//        navigationController.transitioningDelegate = transitioningDelegate
-//
-//        presentingViewController?.present(navigationController, animated: true, completion: nil)
-//    }
-
-//    override func start() {
-//        let viewController = MovieReviewDetailViewController.instantiate()
-//
-//        viewController.viewModel = DIContainer.shared.resolve(argument: review)
-//        viewController.coordinator = self
-//
-//        navigationController.pushViewController(viewController, animated: false)
-//        navigationController.modalPresentationStyle = .fullScreen
-//        navigationController.transitioningDelegate = transitioningDelegate
-//
-//        presentingViewController?.present(navigationController, animated: true, completion: nil)
-//    }
-
-    override func dismiss() {
-        let presentedViewController = navigationController.topViewController
-        presentedViewController?.dismiss(animated: true) { [weak self] in
-            self?.parentCoordinator?.childDidFinish()
-        }
     }
 
 }
