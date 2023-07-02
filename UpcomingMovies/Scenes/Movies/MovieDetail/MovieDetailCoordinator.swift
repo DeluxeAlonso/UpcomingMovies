fca606413d0c3bb07249cf0f9a0fc6d10e6c52f7
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-07-02 00:22:02 -0500

    Removes todo comment in movie detail coordinator

--- UpcomingMovies/Scenes/Movies/MovieDetail/MovieDetailCoordinator.swift
+++ UpcomingMovies/Scenes/Movies/MovieDetail/MovieDetailCoordinator.swift
@@ -16,7 +16,6 @@ enum MovieDetailInfo {
 
 }
 
-// TODO: - Adds unit tests for MovieDetailCoordinator
 final class MovieDetailCoordinator: BaseCoordinator, MovieDetailCoordinatorProtocol {
 
     private let movieInfo: MovieDetailInfo
