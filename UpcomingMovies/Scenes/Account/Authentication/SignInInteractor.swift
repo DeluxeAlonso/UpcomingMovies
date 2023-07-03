57da41adaa4bfa8251d683d343b850664da13a61
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-07-03 00:45:26 -0500

    Implements sign in interactor

---
+++ UpcomingMovies/Scenes/Account/Authentication/SignInInteractor.swift
@@ -0,0 +1,42 @@
+//
+//  SignInInteractor.swift
+//  UpcomingMovies
+//
+//  Created by Alonso on 3/07/23.
+//  Copyright © 2023 Alonso. All rights reserved.
+//
+
+import Foundation
+import UpcomingMoviesDomain
+
+final class SignInInteractor: SignInInteractorProtocol {
+
+    private let authUseCase: AuthUseCaseProtocol
+    private let userUseCase: UserUseCaseProtocol
+
+    init(useCaseProvider: UseCaseProviderProtocol) {
+        self.authUseCase = useCaseProvider.authUseCase()
+        self.userUseCase = useCaseProvider.userUseCase()
+    }
+
+    func getAuthPermissionURL(completion: @escaping (Result<URL, Error>) -> Void) {
+        authUseCase.getAuthURL(completion: completion)
+    }
+
+    func signInUser(completion: @escaping (Result<User, Error>) -> Void) {
+        authUseCase.signInUser { result in
+            switch result {
+            case .success(let user):
+                self.userUseCase.saveUser(user)
+                completion(.success(user))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+
+    func signOutUser(completion: @escaping (Result<Bool, Error>) -> Void) {
+        authUseCase.signOutUser(completion: completion)
+    }
+
+}
