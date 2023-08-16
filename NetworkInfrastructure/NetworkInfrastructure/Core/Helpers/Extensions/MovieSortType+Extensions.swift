531eaa94d76332cd1f365e75c75dd5901b33eca5
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-08-16 00:49:27 -0500

    Implements extension for MovieSortType.Favorite

--- NetworkInfrastructure/NetworkInfrastructure/Core/Helpers/Extensions/MovieSortType+Extensions.swift
+++ NetworkInfrastructure/NetworkInfrastructure/Core/Helpers/Extensions/MovieSortType+Extensions.swift
@@ -19,30 +19,13 @@ extension MovieSortType.Watchlist {
 
 }
 
-//public struct MovieSortType {
-//
-//    public enum Watchlist {
-//        case createdAtAsc
-//        case createdAtDesc
-//
-//        public func callAsFunction() -> String {
-//            switch self {
-//            case .createdAtAsc: return "created_at.asc"
-//            case .createdAtDesc: return "created_at.desc"
-//            }
-//        }
-//    }
-//
-//    public enum Favorite {
-//        case createdAtAsc
-//        case createdAtDesc
-//
-//        public func callAsFunction() -> String {
-//            switch self {
-//            case .createdAtAsc: return "created_at.asc"
-//            case .createdAtDesc: return "created_at.desc"
-//            }
-//        }
-//    }
-//
-//}
+extension MovieSortType.Favorite {
+
+    public func callAsFunction() -> String {
+        switch self {
+        case .createdAtAsc: return "created_at.asc"
+        case .createdAtDesc: return "created_at.desc"
+        }
+    }
+
+}
