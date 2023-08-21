04a671bc967ea4458b766a1391a84a32597c0b0b
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-08-21 00:59:02 -0500

    Adds unit tests for watchlist movie sort type

--- NetworkInfrastructure/MovieSortTypeTests.swift
+++ NetworkInfrastructure/MovieSortTypeTests.swift
@@ -5,4 +5,28 @@
 //  Created by Alonso on 21/08/23.
 //
 
-import Foundation
+import XCTest
+import UpcomingMoviesDomain
+@testable import NetworkInfrastructure
+
+class MovieSortTypeTests: XCTestCase {
+
+    func testWatchlistSortTypeCallAsFunctionCreatedAtAsc() throws {
+        // Arrange
+        let sortType = MovieSortType.Watchlist.createdAtAsc
+        // Act
+        let callAsFunctionResult = sortType()
+        // Assert
+        XCTAssertEqual(callAsFunctionResult, "created_at.asc")
+    }
+
+    func testWatchlistSortTypeCallAsFunctionCreatedAtDesc() throws {
+        // Arrange
+        let sortType = MovieSortType.Watchlist.createdAtDesc
+        // Act
+        let callAsFunctionResult = sortType()
+        // Assert
+        XCTAssertEqual(callAsFunctionResult, "created_at.desc")
+    }
+
+}
