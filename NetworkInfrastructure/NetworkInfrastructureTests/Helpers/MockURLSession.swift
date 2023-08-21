286cf0f3daa4ab15591e0c7d88d8bc77aea44e0a
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-08-21 16:05:02 -0500

    Implements MockURLSession

---
+++ NetworkInfrastructure/NetworkInfrastructureTests/Helpers/MockURLSession.swift
@@ -0,0 +1,23 @@
+//
+//  MockURLSession.swift
+//  NetworkInfrastructure-Unit-NetworkInfrastructureTests
+//
+//  Created by Alonso on 21/08/23.
+//
+
+import Foundation
+
+final class MockURLSession: URLSession {
+
+    var dataTaskWithRequestCompletionHandler: (Data?, URLResponse?, Error?) = (nil, nil, nil)
+    var dataTaskWithRequestResult = URLSessionDataTask()
+    private(set) var dataTaskWithRequestCallCount = 0
+    override func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        dataTaskWithRequestCallCount += 1
+        completionHandler(dataTaskWithRequestCompletionHandler.0,
+                          dataTaskWithRequestCompletionHandler.1,
+                          dataTaskWithRequestCompletionHandler.2)
+        return dataTaskWithRequestResult
+    }
+
+}
