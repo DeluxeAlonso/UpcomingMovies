31c0ea120277a8896874548b5fa50372dfe3926d
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-08-23 00:33:03 -0500

    Updates MovieClient initializer

--- NetworkInfrastructure/Services/MovieClientTests.swift
+++ NetworkInfrastructure/Services/MovieClientTests.swift
@@ -6,15 +6,23 @@
 //
 
 import XCTest
+@testable import NetworkInfrastructure
 
 final class MovieClientTests: XCTestCase {
 
+    private var urlSession: MockURLSession!
+    private var movieClient: MovieClient
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        urlSession = MockURLSession()
+        movieClient = Mo
+
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        urlSession = nil
+        try super.tearDownWithError()
     }
 
     func testExample() throws {
