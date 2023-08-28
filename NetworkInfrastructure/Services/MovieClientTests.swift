d41536934257cd384f243d56f54465dc7f22639b
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-08-28 01:40:44 -0500

    Adds unit tests for similar movies in movie client

--- NetworkInfrastructure/Services/MovieClientTests.swift
+++ NetworkInfrastructure/Services/MovieClientTests.swift
@@ -149,4 +149,45 @@ final class MovieClientTests: XCTestCase {
         wait(for: [expectation], timeout: 1.0)
     }
 
+    func testGetSimilarMoviesError() throws {
+        // Arrange
+        urlSession.dataTaskWithRequestCompletionHandler = (nil, nil, nil)
+        let expectation = XCTestExpectation(description: "Get top rated movies error")
+        // Act
+        movieClient.getSimilarMovies(page: 1, movieId: 1) { result in
+            switch result {
+            case .success:
+                XCTFail("Get top rated movies success")
+            case .failure:
+                break
+            }
+            expectation.fulfill()
+        }
+        // Assert
+        wait(for: [expectation], timeout: 1.0)
+    }
+
+    func testGetSimilarMoviesSuccess() throws {
+        // Arrange
+        let data = try JSONEncoder().encode(MovieResult(results: [], currentPage: 1, totalPages: 1))
+        guard let url = URL(string: "www.google.com") else {
+            XCTFail("Invalid URL")
+            return
+        }
+        urlSession.dataTaskWithRequestCompletionHandler = (data, HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil), nil)
+        let expectation = XCTestExpectation(description: "Get similar movies success")
+        // Act
+        movieClient.getSimilarMovies(page: 1, movieId: 1) { result in
+            switch result {
+            case .success:
+                break
+            case .failure:
+                XCTFail("Get similar movies error")
+            }
+            expectation.fulfill()
+        }
+        // Assert
+        wait(for: [expectation], timeout: 1.0)
+    }
+
 }
