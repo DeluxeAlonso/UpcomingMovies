9cb622dfe7977af5cf0eecde146b2f23ad76f416
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-09-15 01:03:19 -0500

    Implements ConfigurationClientProtocolMock

--- NetworkInfrastructure/NetworkInfrastructure/TestDoubles/ConfigurationClientProtocolMock.swift
+++ NetworkInfrastructure/NetworkInfrastructure/TestDoubles/ConfigurationClientProtocolMock.swift
@@ -5,4 +5,17 @@
 //  Created by Alonso on 15/09/23.
 //
 
-import Foundation
+import UpcomingMoviesDomain
+
+final class ConfigurationClientProtocolMock: ConfigurationClientProtocol {
+
+    var getImagesConfigurationResult: Result<ImagesConfigurationResult, APIError>?
+    private(set) var getImagesConfigurationCallCount = 0
+    func getImagesConfiguration(completion: @escaping (Result<ImagesConfigurationResult, APIError>) -> Void) {
+        if let getImagesConfigurationResult = getImagesConfigurationResult {
+            completion(getImagesConfigurationResult)
+        }
+        getImagesConfigurationCallCount += 1
+    }
+
+}
