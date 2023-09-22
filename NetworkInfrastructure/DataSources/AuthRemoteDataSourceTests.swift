1a3f5fa115f20b838b95e19a7d587c0e1a9d5915
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-09-22 01:02:09 -0500

    Creates file for AuthRemoteDataSourceTests

---
+++ NetworkInfrastructure/DataSources/AuthRemoteDataSourceTests.swift
@@ -0,0 +1,33 @@
+//
+//  AuthRemoteDataSourceTests.swift
+//  NetworkInfrastructure-Unit-NetworkInfrastructureTests
+//
+//  Created by Alonso on 22/09/23.
+//
+
+import XCTest
+@testable import UpcomingMoviesDomain
+@testable import NetworkInfrastructure
+
+final class AuthRemoteDataSourceTests: XCTestCase {
+
+    var dataSource: AuthRemoteDataSource!
+    var authClient: AuthClientProtocolMock!
+    var accountClient: AccountClientProtocolMock!
+    var authManager: AuthenticationManagerProtocolMock!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        authClient = AuthClientProtocolMock()
+        accountClient = AccountClientProtocolMock()
+        authManager = AuthenticationManagerProtocolMock()
+        dataSource = AuthRemoteDataSource(client: accountClient, authManager: authManager)
+    }
+
+    override func tearDownWithError() throws {
+        authClient = nil
+        authManager = nil
+        accountClient = nil
+        dataSource = nil
+        try super.tearDownWithError()
+    }
