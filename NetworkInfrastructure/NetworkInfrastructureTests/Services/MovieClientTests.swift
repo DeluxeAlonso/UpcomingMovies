e87ce51e627d64ec44533480fe8e8df4e53604cf
DeluxeAlonso <alonso.alvarez@pucp.pe>
2023-09-30 21:19:28 -0500

    Updates MovieClientTests

--- NetworkInfrastructure/NetworkInfrastructureTests/Services/MovieClientTests.swift
+++ NetworkInfrastructure/NetworkInfrastructureTests/Services/MovieClientTests.swift
@@ -383,7 +383,7 @@ final class MovieClientTests: XCTestCase {
 
     func testGetMovieReviewsSuccess() throws {
         // Arrange
-        let data = try JSONEncoder().encode(ReviewResult(results: [], currentPage: 1, totalPages: 1))
+        let data = MockResponse.reviewResult.dataResponse
         guard let url = URL(string: "www.google.com") else {
             XCTFail("Invalid URL")
             return
