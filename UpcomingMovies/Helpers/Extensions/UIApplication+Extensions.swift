b3b12e0cfcf677be3c310374f850884ad6375842
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-01-21 23:11:05 -0500

    Fixes minor swiftlint warnings

--- UpcomingMovies/Helpers/Extensions/UIApplication+Extensions.swift
+++ UpcomingMovies/Helpers/Extensions/UIApplication+Extensions.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 extension UIApplication {
-    
+
     var keyWindow: UIWindow? {
         connectedScenes
             .filter { $0.activationState == .foregroundActive }
@@ -17,5 +17,5 @@ extension UIApplication {
             .flatMap({ $0 as? UIWindowScene })?.windows
             .first(where: \.isKeyWindow)
     }
-    
+
 }
