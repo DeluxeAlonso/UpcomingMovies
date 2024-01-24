e97ec0a06476743bd02ba042ad92f4b8ce8dc61f
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-01-24 00:01:25 -0500

    Updates List entity

--- UpcomingMoviesDomain/UpcomingMoviesDomain/Core/Entities/List.swift
+++ UpcomingMoviesDomain/UpcomingMoviesDomain/Core/Entities/List.swift
@@ -13,12 +13,12 @@ public struct List: Equatable {
     public let description: String?
     public let backdropPath: String?
     public let averageRating: Double?
-    public let runtime: Int?
+    public let runtime: String?
     public let movieCount: Int
     public let movies: [Movie]?
 
     public init(id: String, name: String, description: String?, backdropPath: String?,
-                averageRating: Double?, runtime: Int?, movieCount: Int, movies: [Movie]?) {
+                averageRating: Double?, runtime: String?, movieCount: Int, movies: [Movie]?) {
         self.id = id
         self.name = name
         self.description = description
