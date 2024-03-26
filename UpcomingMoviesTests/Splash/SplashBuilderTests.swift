6da80cec268ef1e723f3dc04764dfbe8b7f516e7
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-03-25 20:02:49 -0500

    Adds unit tests for SplashBuilderTests

--- UpcomingMoviesTests/Splash/SplashBuilderTests.swift
+++ UpcomingMoviesTests/Splash/SplashBuilderTests.swift
@@ -11,27 +11,17 @@ import XCTest
 
 final class SplashBuilderTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testBuildViewController() {
+        // Act
+        let tabBarController = SplashBuilder.buildViewController() as? UITabBarController
+        let navigationController = tabBarController?.viewControllers?.first as? UINavigationController
+        let splashViewController = navigationController?.viewControllers.first as? SplashViewController
+        // Assert
+        XCTAssertEqual(tabBarController?.viewControllers?.count, 1)
+        XCTAssertEqual(navigationController?.viewControllers.count, 1)
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        XCTAssertNotNil(splashViewController?.viewModel)
+        XCTAssertNotNil(splashViewController?.navigationHandler)
     }
 
 }
