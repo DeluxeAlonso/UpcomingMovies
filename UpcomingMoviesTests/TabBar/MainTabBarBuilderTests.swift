3984d2c2e85875d5d78c711e5f69eb537cd2ceb7
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-03-26 23:22:23 -0500

    Adds unit tests for MainTabBarBuilder

--- UpcomingMoviesTests/TabBar/MainTabBarBuilderTests.swift
+++ UpcomingMoviesTests/TabBar/MainTabBarBuilderTests.swift
@@ -7,30 +7,29 @@
 //
 
 import XCTest
+@testable import UpcomingMovies
 
 final class MainTabBarBuilderTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testCreateNavigationController() {
+        // Arrange
+        let title = "Title"
+        let image = UIImage()
+        // Act
+        let navigationController = MainTabBarBuilder.createNavigationController(title: title, image: image)
+        // Assert
+        XCTAssertEqual(navigationController.tabBarItem.title, title)
+        XCTAssertEqual(navigationController.tabBarItem.image, image)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testBuildViewCoordinators() {
+        // Act
+        let coordinators = MainTabBarBuilder.buildViewCoordinators()
+        // Assert
+        XCTAssertEqual(coordinators.count, 3)
+        XCTAssertNotNil(coordinators[0] as? UpcomingMoviesCoordinator)
+        XCTAssertNotNil(coordinators[1] as? SearchMoviesCoordinator)
+        XCTAssertNotNil(coordinators[2] as? AccountCoordinator)
     }
 
 }
