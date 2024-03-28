bc26630f669f4ccc5782d6832a0c6e54e2c0385f
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-03-27 23:46:47 -0500

    Creates file for MovieCellViewModelTests

---
+++ UpcomingMoviesTests/Movies/MovieList/Cells/MovieCellViewModelTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCellViewModelTests.swift
+//  UpcomingMoviesTests
+//
+//  Created by Alonso on 27/03/24.
+//  Copyright © 2024 Alonso. All rights reserved.
+//
+
+import XCTest
+@testable import UpcomingMovies
+import UpcomingMoviesDomain
+
+final class MovieCellViewModelTests: XCTestCase {
+
+    
+
+}
