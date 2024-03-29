19dfba4fbbd8b6dcc2e9eb9e4c7eb46dce8ad974
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-03-28 23:06:19 -0500

    Implements MovieProtocol

---
+++ UpcomingMovies/Helpers/Protocols/Models/MovieProtocol.swift
@@ -0,0 +1,20 @@
+//
+//  MovieProtocol.swift
+//  UpcomingMovies
+//
+//  Created by Alonso on 28/03/24.
+//  Copyright © 2024 Alonso. All rights reserved.
+//
+
+import Foundation
+
+protocol MovieProtocol: GenreNameable {
+    var id: Int { get }
+    var title: String { get }
+    var genreIds: [Int]? { get }
+    var overview: String { get }
+    var posterPath: String? { get }
+    var backdropPath: String? { get }
+    var releaseDate: String? { get }
+    var voteAverage: Double? { get }
+}
