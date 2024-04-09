1b6b1d6bb08866e0414304c03569dce6ef021ac8
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-04-09 00:12:20 -0500

    Adds tests for MovieDetailTitleRenderContent

--- UpcomingMoviesTests/Movies/MovieDetail/MovieDetailTitle/MovieDetailTitleRenderContentTests.swift
+++ UpcomingMoviesTests/Movies/MovieDetail/MovieDetailTitle/MovieDetailTitleRenderContentTests.swift
@@ -6,31 +6,54 @@
 //  Copyright © 2024 Alonso. All rights reserved.
 //
 
+@testable import UpcomingMovies
+import UpcomingMoviesDomain
 import XCTest
 
 final class MovieDetailTitleRenderContentTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testTitle() {
+        // Arrange
+        let movie = MockMovieProtocol()
+        movie.title = "Title"
+        // Act
+        let renderContent = MovieDetailTitleRenderContent(movie: movie)
+        let title = renderContent.title
+        // Assert
+        XCTAssertEqual("Title", title)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testReleaseDate() {
+        // Arrange
+        let movie = MockMovieProtocol()
+        movie.releaseDate = "17-04-1992"
+        // Act
+        let renderContent = MovieDetailTitleRenderContent(movie: movie)
+        let releaseDate = renderContent.releaseDate
+        // Assert
+        XCTAssertEqual("17-04-1992", releaseDate)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testVoteAverage() {
+        // Arrange
+        let movie = MockMovieProtocol()
+        movie.voteAverage = 5.5
+        // Act
+        let renderContent = MovieDetailTitleRenderContent(movie: movie)
+        let voteAverage = renderContent.voteAverage
+        // Assert
+        XCTAssertEqual(5.5, voteAverage)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testGenreIds() {
+        // Arrange
+        let movie = MockMovieProtocol()
+        movie.genreIds = [1]
+        // Act
+        let renderContent = MovieDetailTitleRenderContent(movie: movie)
+        let genreIds = renderContent.genreIds
+        // Assert
+        XCTAssertEqual([1], genreIds)
     }
 
 }
