e4806b178d23af8c91323b2058fb909d409441a2
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-04-23 00:31:10 -0500

    Implements ReviewProtocol

---
+++ UpcomingMovies/Helpers/Protocols/Models/ReviewProtocol.swift
@@ -0,0 +1,19 @@
+//
+//  ReviewProtocol.swift
+//  UpcomingMovies
+//
+//  Created by Alonso on 23/04/24.
+//  Copyright © 2024 Alonso. All rights reserved.
+//
+
+import UpcomingMoviesDomain
+
+protocol ReviewProtocol {
+
+    var id: String { get }
+    var authorName: String { get }
+    var content: String { get }
+
+}
+
+extension Review: ReviewProtocol {}
