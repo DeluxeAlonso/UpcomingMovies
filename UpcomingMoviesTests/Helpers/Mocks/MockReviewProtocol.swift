92d615d5bffec1b146634f648df660dec8c0d2b8
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-05-04 23:50:38 -0500

    Updates MockReviewProtocol

--- UpcomingMoviesTests/Helpers/Mocks/MockReviewProtocol.swift
+++ UpcomingMoviesTests/Helpers/Mocks/MockReviewProtocol.swift
@@ -9,9 +9,9 @@
 @testable import UpcomingMovies
 
 final class MockReviewProtocol: ReviewProtocol {
-    var id: String = ""
-    var authorName: String = ""
-    var content: String = ""
+    var id: String
+    var authorName: String
+    var content: String
 
     init(id: String = "", authorName: String = "", content: String = "") {
         self.id = id
