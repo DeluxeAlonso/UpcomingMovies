2415c886268ab704e1a42ee8ea2a19b44b402a28
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-05-05 02:47:23 -0500

    Adds MockMovieProtocol initializer

--- UpcomingMoviesTests/Helpers/Mocks/MockMovieProtocol.swift
+++ UpcomingMoviesTests/Helpers/Mocks/MockMovieProtocol.swift
@@ -10,14 +10,35 @@ import Foundation
 @testable import UpcomingMovies
 
 final class MockMovieProtocol: MovieProtocol {
-    var id: Int = 0
-    var title: String = ""
+
+    var id: Int
+    var title: String
     var genreIds: [Int]?
-    var overview: String = ""
+    var overview: String
     var releaseDate: String?
     var voteAverage: Double?
     var posterURL: URL?
     var backdropURL: URL?
 
-    var genreName: String = ""
+    var genreName: String
+
+    init(id: Int = 0,
+         title: String = "",
+         genreIds: [Int]? = nil,
+         overview: String = "",
+         releaseDate: String? = nil,
+         voteAverage: Double? = nil,
+         posterURL: URL? = nil,
+         backdropURL: URL? = nil,
+         genreName: String = "") {
+        self.id = id
+        self.title = title
+        self.genreIds = genreIds
+        self.overview = overview
+        self.releaseDate = releaseDate
+        self.voteAverage = voteAverage
+        self.posterURL = posterURL
+        self.backdropURL = backdropURL
+        self.genreName = genreName
+    }
 }
