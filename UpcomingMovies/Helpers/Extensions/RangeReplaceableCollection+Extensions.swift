f19f65e76f57bdcf8d06de221e153be232fe64c0
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-06-05 00:43:45 -0500

    Removes unused method in RangeReplaceableCollection+Extensions

--- UpcomingMovies/Helpers/Extensions/RangeReplaceableCollection+Extensions.swift
+++ UpcomingMovies/Helpers/Extensions/RangeReplaceableCollection+Extensions.swift
@@ -10,14 +10,6 @@ import Foundation
 
 extension RangeReplaceableCollection {
 
-     mutating func remove(while predicate: (Element) throws -> Bool) rethrows {
-        guard let index = try indices.first(where: { try !predicate(self[$0]) }) else {
-            removeAll()
-            return
-        }
-        removeSubrange(..<index)
-    }
-
     mutating func removeLast(while predicate: (Element) throws -> Bool) rethrows {
         guard let index = try indices.reversed().first(where: { try !predicate(self[$0]) }) else {
             removeAll()
