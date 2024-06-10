e3b53231a5510877404833ac8ef7d085a0dbea04
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-06-09 22:03:10 -0500

    Updates MovieVideoCellViewModelTests to use VideoProtocol

--- UpcomingMoviesTests/Movies/MovieVideos/MovieVideoCellViewModelTests.swift
+++ UpcomingMoviesTests/Movies/MovieVideos/MovieVideoCellViewModelTests.swift
@@ -10,30 +10,30 @@ import XCTest
 @testable import UpcomingMovies
 import UpcomingMoviesDomain
 
-//final class MovieVideoCellViewModelTests: XCTestCase {
-//
-//    func testNameForVideoModel() {
-//        // Arrange
-//        let nameToTest = "Video1"
-//        let viewModel = createSUT(with: Video.with(name: nameToTest))
-//        // Act
-//        let viewModelName = viewModel.name
-//        // Assert
-//        XCTAssertEqual(nameToTest, viewModelName)
-//    }
-//
-//    func testThumbnailURLForVideoModel() {
-//        // Arrange
-//        let urlToTest = URL(string: "google.com")
-//        let viewModel = createSUT(with: Video.with(thumbnailURL: urlToTest))
-//        // Act
-//        let viewModelThumbnailURL = viewModel.thumbnailURL
-//        // Assert
-//        XCTAssertEqual(urlToTest, viewModelThumbnailURL)
-//    }
-//
-//    private func createSUT(with video: Video) -> MovieVideoCellViewModel {
-//        MovieVideoCellViewModel(video)
-//    }
-//
-//}
+final class MovieVideoCellViewModelTests: XCTestCase {
+
+    func testNameForVideoModel() {
+        // Arrange
+        let nameToTest = "Video1"
+        let viewModel = createSUT(with: MockVideoProtocol(name: nameToTest))
+        // Act
+        let viewModelName = viewModel.name
+        // Assert
+        XCTAssertEqual(nameToTest, viewModelName)
+    }
+
+    func testThumbnailURLForVideoModel() {
+        // Arrange
+        let urlToTest = URL(string: "google.com")
+        let viewModel = createSUT(with: MockVideoProtocol(thumbnailURL: urlToTest))
+        // Act
+        let viewModelThumbnailURL = viewModel.thumbnailURL
+        // Assert
+        XCTAssertEqual(urlToTest, viewModelThumbnailURL)
+    }
+
+    private func createSUT(with video: VideoProtocol) -> MovieVideoCellViewModel {
+        MovieVideoCellViewModel(video)
+    }
+
+}
