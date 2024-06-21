f81fa6a71590f1cf100b3c60aad1f1960eaffb68
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-06-21 00:14:20 -0500

    Implements MovieModel

--- UpcomingMovies/Helpers/Protocols/Models/MovieProtocol.swift
+++ UpcomingMovies/Helpers/Protocols/Models/MovieProtocol.swift
@@ -46,3 +46,51 @@ extension Movie: MovieProtocol {
     }
 
 }
+
+struct MovieModel: MovieProtocol {
+
+    let id: Int
+    let title: String
+    let genreIds: [Int]?
+    let overview: String
+    let releaseDate: String?
+    let voteAverage: Double?
+    let posterPath: String?
+    let backdropPath: String?
+
+    private let genreHandler: GenreHandlerProtocol
+
+    var posterURL: URL? {
+        guard let posterPath = posterPath else { return nil }
+        let urlString = regularImageBaseURLString.appending(posterPath)
+        return URL(string: urlString)
+    }
+
+    var backdropURL: URL? {
+        guard let backdropPath = backdropPath else { return nil }
+        let urlString = backdropImageBaseURLString.appending(backdropPath)
+        return URL(string: urlString)
+    }
+
+    var genreName: String {
+        guard let genreId = genreIds?.first,
+              let genreName = genreHandler.getGenreName(for: genreId) else {
+            return "-"
+        }
+        return genreName
+    }
+
+    init(_ movie: Movie,
+         genreHandler: GenreHandlerProtocol = DIContainer.shared.resolve()) {
+        self.id = movie.id
+        self.title = movie.title
+        self.genreIds = movie.genreIds
+        self.overview = movie.overview
+        self.releaseDate = movie.releaseDate
+        self.voteAverage = movie.voteAverage
+        self.posterPath = movie.posterPath
+        self.backdropPath = movie.backdropPath
+
+        self.genreHandler = genreHandler
+    }
+}
