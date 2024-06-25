0a615a5159353c7d83d87f8e2c75fc374954085f
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-06-24 22:42:27 -0500

    Creates CrewModelTests file

---
+++ UpcomingMoviesTests/CrewModelTests.swift
@@ -0,0 +1,18 @@
+//
+//  CrewModelTests.swift
+//  UpcomingMoviesTests
+//
+//  Created by Alonso on 24/06/24.
+//  Copyright © 2024 Alonso. All rights reserved.
+//
+
+import XCTest
+@testable import UpcomingMovies
+import UpcomingMoviesDomain
+
+final class CrewModelTests: XCTestCase {
+
+    
+
+}
+
