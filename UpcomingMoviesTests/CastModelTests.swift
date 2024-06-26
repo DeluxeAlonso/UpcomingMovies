b5a38babe4f1d444e196486b0d3dd798af5e4fb5
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-06-25 23:49:30 -0500

    Implements CastModelTests

--- UpcomingMoviesTests/CastModelTests.swift
+++ UpcomingMoviesTests/CastModelTests.swift
@@ -7,30 +7,21 @@
 //
 
 import XCTest
+@testable import UpcomingMovies
+import UpcomingMoviesDomain
 
 final class CastModelTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testInitWithCast() {
+        // Arrange
+        let cast = Cast.with(id: 12345, character: "Character", name: "Name", photoPath: "/path")
+        // Act
+        let model = CastModel(cast)
+        // Assert
+        XCTAssertEqual(model.id, 12345)
+        XCTAssertEqual(model.character, "Character")
+        XCTAssertEqual(model.name, "Name")
+        XCTAssertEqual(model.profileURL?.absoluteString, "https://image.tmdb.org/t/p/w342/path")
     }
 
 }
--- UpcomingMoviesTests/CrewModelTests.swift
+++ UpcomingMoviesTests/CrewModelTests.swift
@@ -25,4 +25,3 @@ final class CrewModelTests: XCTestCase {
     }
 
 }
-
