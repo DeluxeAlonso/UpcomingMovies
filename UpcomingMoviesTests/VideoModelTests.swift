20bf3d670c2c2161f5e8ee3bc09ecb5fc8e7d6e5
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-06-27 00:15:13 -0500

    Implements VideoModelTests

--- UpcomingMoviesTests/VideoModelTests.swift
+++ UpcomingMoviesTests/VideoModelTests.swift
@@ -7,30 +7,30 @@
 //
 
 import XCTest
+@testable import UpcomingMovies
+import UpcomingMoviesDomain
 
 final class VideoModelTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testInitWithCast() {
+        // Arrange
+        let video = Video.with(id: "12345",
+                              key: "Key",
+                              name: "Name",
+                              site: "Site",
+                              browserURL: URL(string: "www.google.com"),
+                              deepLinkURL: URL(string: "www.google.com"),
+                              thumbnailURL: URL(string: "www.google.com"))
+        // Act
+        let model = VideoModel(video)
+        // Assert
+        XCTAssertEqual(model.id, "12345")
+        XCTAssertEqual(model.key, "Key")
+        XCTAssertEqual(model.name, "Name")
+        XCTAssertEqual(model.site, "Site")
+        XCTAssertEqual(model.browserURL?.absoluteString, "www.google.com")
+        XCTAssertEqual(model.deepLinkURL?.absoluteString, "www.google.com")
+        XCTAssertEqual(model.thumbnailURL?.absoluteString, "www.google.com")
     }
 
 }
