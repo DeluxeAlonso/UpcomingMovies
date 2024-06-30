ceb1207502bf58bb760859e061f766ebdf802869
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-06-30 15:24:11 -0500

    Implements MockConfigurationHandlerProtocol

---
+++ UpcomingMoviesTests/Helpers/Mocks/MockConfigurationHandlerProtocol.swift
@@ -0,0 +1,29 @@
+//
+//  MockConfigurationHandlerProtocol.swift
+//  UpcomingMoviesTests
+//
+//  Created by Alonso on 30/06/24.
+//  Copyright © 2024 Alonso. All rights reserved.
+//
+
+import Foundation
+@testable import UpcomingMovies
+import UpcomingMoviesDomain
+
+final class MockConfigurationHandlerProtocol: ConfigurationHandlerProtocol {
+
+    var regularImageBaseURLString: String
+    var backdropImageBaseURLString: String
+    var avatarImageBaseURLString: String?
+
+    init(regularImageBaseURLString: String = "https://image.tmdb.org/t/p/regular",
+         backdropImageBaseURLString: String = "https://image.tmdb.org/t/p/backdrop",
+         avatarImageBaseURLString: String? = "https://image.tmdb.org/t/p/avatar") {
+        self.regularImageBaseURLString = regularImageBaseURLString
+        self.backdropImageBaseURLString = backdropImageBaseURLString
+        self.avatarImageBaseURLString = avatarImageBaseURLString
+    }
+
+    func setConfiguration(_ configuration: Configuration) {}
+
+}
