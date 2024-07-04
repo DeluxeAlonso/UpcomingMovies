be3fa5d95fcb219418126591ef60bb5f0161b939
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-07-04 00:26:00 -0500

    Deletes UIKit import from MovieVideosViewModel

--- UpcomingMovies/Scenes/Movies/MovieVideos/MovieVideosViewModel.swift
+++ UpcomingMovies/Scenes/Movies/MovieVideos/MovieVideosViewModel.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Alonso. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
 final class MovieVideosViewModel: MovieVideosViewModelProtocol, SimpleViewStateProcessable {
 
