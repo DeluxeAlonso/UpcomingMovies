d34188ecaad575fb44e830af29fc474d9a4bf6c8
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-07-09 00:35:45 -0500

    Implements MovieSearchModel

--- UpcomingMovies/Helpers/Protocols/Models/MovieSearchProtocol.swift
+++ UpcomingMovies/Helpers/Protocols/Models/MovieSearchProtocol.swift
@@ -6,10 +6,25 @@
 //  Copyright © 2024 Alonso. All rights reserved.
 //
 
-import Foundation
+import UpcomingMoviesDomain
 
 protocol MovieSearchProtocol {
+
     var id: String { get }
     var searchText: String { get }
     var createdAt: Date { get }
+}
+
+struct MovieSearchModel: MovieSearchProtocol {
+
+    let id: String
+    let searchText: String
+    let createdAt: Date
+
+    init(_ movieSearch: MovieSearch) {
+        self.id = movieSearch.id
+        self.searchText = movieSearch.searchText
+        self.createdAt = movieSearch.createdAt
+    }
+
 }
