6b6baaadc6ec355ff60f9d0f5e3cdc6379d1a7c1
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-07-10 22:40:39 -0500

    Creates GenreProtocol

---
+++ UpcomingMovies/Helpers/Protocols/Models/GenreProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  GenreProtocol.swift
+//  UpcomingMovies
+//
+//  Created by Alonso on 10/07/24.
+//  Copyright © 2024 Alonso. All rights reserved.
+//
+
+import Foundation
+
+protocol GenreProtocol {
+
+    var id: Int { get }
+    var name: String { get }
+
+}
