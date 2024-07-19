472b46460c29d1a7d31b7c6e4cffeaab6d666008
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-07-19 00:22:14 -0500

    Implements MovieVisitModelTests

--- UpcomingMoviesTests/MovieVisitModelTests.swift
+++ UpcomingMoviesTests/MovieVisitModelTests.swift
@@ -7,30 +7,21 @@
 //
 
 import XCTest
+@testable import UpcomingMovies
+import UpcomingMoviesDomain
 
 final class MovieVisitModelTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testInitWithMovieVisit() {
+        // Arrange
+        let movieVisit = MovieVisit(id: 12345, title: "Title", posterPath: "/path", createdAt: Date(timeIntervalSince1970: 0))
+        // Act
+        let model = MovieVisitModel(movieVisit)
+        // Assert
+        XCTAssertEqual(model.id, 12345)
+        XCTAssertEqual(model.title, "Title")
+        XCTAssertEqual(model.posterPath, "/path")
+        XCTAssertEqual(model.createdAt?.timeIntervalSince1970, 0)
     }
 
 }
