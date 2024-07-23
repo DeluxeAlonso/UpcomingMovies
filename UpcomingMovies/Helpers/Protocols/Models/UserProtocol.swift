1580630ec12e340e3df6c91c38fd3ccfe2a9cebc
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-07-23 01:15:15 -0500

    Updates UserModel

--- UpcomingMovies/Helpers/Protocols/Models/UserProtocol.swift
+++ UpcomingMovies/Helpers/Protocols/Models/UserProtocol.swift
@@ -17,29 +17,38 @@ protocol UserProtocol: ImageConfigurable {
     var includeAdult: Bool { get }
     var avatarImageURL: URL? { get }
 
+    func hasUpdatedInfo(_ newUserInfo: UserProtocol) -> Bool
+
 }
 
 struct UserModel: UserProtocol {
-
+    
     let id: Int
     let name: String
     let username: String
     let includeAdult: Bool
-
+    
     private let avatarPath: String?
-
+    
     var avatarImageURL: URL? {
         guard let avatarPath, let avatarImageBaseURLString else { return nil }
         let urlString = avatarImageBaseURLString.appending(avatarPath)
         return URL(string: urlString)
     }
-
+    
     init(_ user: User) {
         self.id = user.id
         self.name = user.name
         self.username = user.username
         self.includeAdult = user.includeAdult
         self.avatarPath = user.avatarPath
+    }
+    
+    func hasUpdatedInfo(_ newUserInfo: UserProtocol) -> Bool {
+        self.id != newUserInfo.id ||
+        self.name != newUserInfo.name ||
+        self.username != newUserInfo.username ||
+        self.avatarImageURL != newUserInfo.avatarImageURL
     }
 
 }
