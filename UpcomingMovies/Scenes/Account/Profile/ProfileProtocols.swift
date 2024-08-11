c74be7d3960bad03083acaea7b9be12ad7f5c990
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-08-11 01:17:30 -0500

    Updates ProfileProtocols

--- UpcomingMovies/Scenes/Account/Profile/ProfileProtocols.swift
+++ UpcomingMovies/Scenes/Account/Profile/ProfileProtocols.swift
@@ -23,6 +23,7 @@ protocol ProfileViewModelProtocol {
     func profileOption(for section: Int, at index: Int) -> ProfileOptionProtocol
     func buildProfileOptionCellViewModel(for section: Int,
                                          at index: Int) -> ProfileSelectableOptionCellViewModelProtocol
+    func buildSignOutCellViewModel(for section: Int) -> ProfileSignOutCellViewModelProtocol
 
     func getAccountDetails()
     func signOutCurrentUser()
