f3a5ff64adb37d3c35a7054630bbe257101f0183
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-09-28 22:42:23 -0500

    Adds unit tests for MovieCreditsSectionManager

--- UpcomingMoviesTests/Movies/MovieCredits/MovieCreditsSectionManager.swift
+++ UpcomingMoviesTests/Movies/MovieCredits/MovieCreditsSectionManager.swift
@@ -11,21 +11,21 @@ import XCTest
 
 final class MovieCreditsSectionManagerTests: XCTestCase {
 
-    private var factory: MovieCreditsSectionManager!
+    private var manager: MovieCreditsSectionManager!
 
     override func setUpWithError() throws {
         try super.setUpWithError()
-        factory = MovieCreditsSectionManager()
+        manager = MovieCreditsSectionManager()
     }
 
     override func tearDownWithError() throws {
-        factory = nil
+        manager = nil
         try super.tearDownWithError()
     }
 
     func testCastSection() {
         // Act
-        let sections = factory.sections
+        let sections = manager.sections
         let castSection = sections[0]
         // Assert
         XCTAssertEqual(castSection.type, .cast)
@@ -34,11 +34,45 @@ final class MovieCreditsSectionManagerTests: XCTestCase {
 
     func testCrewSection() {
         // Act
-        let sections = factory.sections
+        let sections = manager.sections
         let crewSection = sections[1]
         // Assert
         XCTAssertEqual(crewSection.type, .crew)
         XCTAssertFalse(crewSection.opened)
+    }
+
+    func testCastSectionEnabledFalse() {
+        // Act
+        manager.updateSection(type: .cast, enabled: false)
+        // Assert
+        XCTAssertEqual(manager.sections.count, 1)
+        XCTAssertFalse(manager.sections.contains(where: { $0.type == .cast }))
+    }
+
+    func testCrewSectionEnabledFalse() {
+        // Act
+        manager.updateSection(type: .crew, enabled: false)
+        // Assert
+        XCTAssertEqual(manager.sections.count, 1)
+        XCTAssertFalse(manager.sections.contains(where: { $0.type == .crew }))
+    }
+
+    func testCastSectionToggle() {
+        // Act
+        manager.toggleSection(at: 0)
+        let sections = manager.sections
+        let castSection = sections[0]
+        // Assert
+        XCTAssertFalse(castSection.opened)
+    }
+
+    func testCrewSectionToggle() {
+        // Act
+        manager.toggleSection(at: 1)
+        let sections = manager.sections
+        let castSection = sections[1]
+        // Assert
+        XCTAssertTrue(castSection.opened)
     }
 
 }
