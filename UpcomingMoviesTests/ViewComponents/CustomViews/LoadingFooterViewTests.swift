f586e9e6539c91abc40e74c0d8cfda757ea17c4a
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-10-06 23:33:33 -0500

    Adds initializer test for LoadingFooterView

--- UpcomingMoviesTests/ViewComponents/CustomViews/LoadingFooterViewTests.swift
+++ UpcomingMoviesTests/ViewComponents/CustomViews/LoadingFooterViewTests.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2024 Alonso. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import UpcomingMovies
+
+final class LoadingFooterViewTests: XCTestCase {
+
+    func testInit() {
+        // Arrange
+        let view = LoadingFooterView()
+        let recommendedFrame = LoadingFooterView.recommendedFrame
+        // Act
+        let frame = view.frame
+        // Assert
+        XCTAssertEqual(frame, recommendedFrame)
+    }
+
+    func testInitWithFrame() {
+        // Arrange
+        // Act
+        // Assert
+    }
+}
