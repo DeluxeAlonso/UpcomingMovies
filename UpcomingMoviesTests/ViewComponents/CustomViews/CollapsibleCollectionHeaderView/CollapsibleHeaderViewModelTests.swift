3bc407fb0d8b6ce1c4ab102c52c3038a70d50640
DeluxeAlonso <alonso.alvarez@pucp.pe>
2024-10-10 23:43:05 -0500

    Adds unit tests for CollapsibleHeaderViewModel

--- UpcomingMoviesTests/ViewComponents/CustomViews/CollapsibleCollectionHeaderView/CollapsibleHeaderViewModelTests.swift
+++ UpcomingMoviesTests/ViewComponents/CustomViews/CollapsibleCollectionHeaderView/CollapsibleHeaderViewModelTests.swift
@@ -7,30 +7,32 @@
 //
 
 import XCTest
+@testable import UpcomingMovies
 
 final class CollapsibleHeaderViewModelTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testInitWithDefaultShouldAnimate() {
+        // Act
+        let viewModel = CollapsibleHeaderViewModel(opened: true, section: 0, title: "Title")
+        // Assert
+        XCTAssertEqual(viewModel.opened, true)
+        XCTAssertEqual(viewModel.section, 0)
+        XCTAssertEqual(viewModel.title, "Title")
+        XCTAssertEqual(viewModel.shouldAnimate, false)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testInitWithShouldAnimateTrue() {
+        // Act
+        let viewModel = CollapsibleHeaderViewModel(opened: true, section: 0, title: "Title", shouldAnimate: true)
+        // Assert
+        XCTAssertEqual(viewModel.shouldAnimate, true)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testInitWithShouldAnimateFalse() {
+        // Act
+        let viewModel = CollapsibleHeaderViewModel(opened: true, section: 0, title: "Title", shouldAnimate: false)
+        // Assert
+        XCTAssertEqual(viewModel.shouldAnimate, false)
     }
 
 }
